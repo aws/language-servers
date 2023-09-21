a1b38398f21a7475ba032df27dbae333b1d9910c
Ege Ozcan <eoozca@amazon.com>
2023-09-21 14:39:09 +0200

    Make token based server the default

--- app/aws-lsp-codewhisperer-binary/src/index.ts
+++ app/aws-lsp-codewhisperer-binary/src/index.ts
@@ -1,4 +1,4 @@
 import { standalone } from '@aws-placeholder/aws-language-server-runtimes/out/runtimes'
-import { CodeWhispererServerIAM } from '@lsp-placeholder/aws-lsp-codewhisperer'
+import { CodeWhispererServerToken } from '@lsp-placeholder/aws-lsp-codewhisperer'
 
-standalone(CodeWhispererServerIAM)
+standalone(CodeWhispererServerToken)
