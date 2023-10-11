9bbb32da960d9e10c78cbc7d01d137fd56e20dd1
Ivan Mykhailovskyi <imykhai@amazon.com>
2023-10-11 15:30:04 +0000

    Align HelloWorld LSP Server with runtimes interfaces

--- app/hello-world-lsp-binary/src/index.ts
+++ app/hello-world-lsp-binary/src/index.ts
@@ -1,4 +1,14 @@
-import { standalone } from '@aws-placeholder/aws-language-server-runtimes/out/runtimes'
-import { HelloWorldServer } from '@lsp-placeholder/hello-world-lsp'
+import { standalone } from '@aws-placeholder/aws-language-server-runtimes/out/runtimes';
+import { RuntimeProps } from '@aws-placeholder/aws-language-server-runtimes/out/runtimes/runtime';
+import { HelloWorldServer } from '@lsp-placeholder/hello-world-lsp';
 
-standalone(HelloWorldServer)
+const MAJOR: number = 0;
+const MINOR: number = 1;
+const PATCH: number = 0;
+const VERSION: string = `${MAJOR}.${MINOR}.${PATCH}`
+
+const props: RuntimeProps = {
+    version: VERSION,
+    servers: [HelloWorldServer]
+}
+standalone(props)
