6fb4bf00aec7e8b3e71e94b75c16fecdea7e778e
Anastasiia Derendeeva <aderende@amazon.com>
2024-02-21 11:41:55 +0100

    Added YAML JSON servers

---
+++ server/aws-lsp-yaml-json/src/language-server/yamlJsonServer.test.ts
@@ -0,0 +1,56 @@
+import { Server } from '@aws/language-server-runtimes'
+import { TestFeatures } from '@aws/language-server-runtimes/out/testing'
+import { AwsLanguageService } from '@aws/lsp-core'
+import sinon, { StubbedInstance, stubInterface } from 'ts-sinon'
+import { CancellationToken } from 'vscode-languageserver'
+import { JSON_COMPLETION_FILE, JSON_COMPLETION_POSITION, JSON_VALIDATION_FILE } from './testUtils'
+import { YamlJsonServerFactory } from './yamlJsonServer'
+export { YamlJsonServerFactory } from './yamlJsonServer'
+
+describe('YamlJson Server', () => {
+    let features: TestFeatures
+    let server: Server
+
+    // TODO: write tests with real YAML/JSON service
+    let service: StubbedInstance<AwsLanguageService>
+
+    beforeEach(async () => {
+        service = stubInterface<AwsLanguageService>()
+
+        server = YamlJsonServerFactory(service)
+
+        // Initialize the features, but don't start server yet
+        features = new TestFeatures()
+
+        // Start the server and open a document
+        await features.start(server)
+
+        features.openDocument(JSON_COMPLETION_FILE)
+    })
+
+    afterEach(() => {
+        features.dispose()
+    })
+
+    it('should validate', async () => {
+        features.workspace.getTextDocument.returns(Promise.resolve(JSON_COMPLETION_FILE))
+
+        await features.doChangeTextDocument({
+            textDocument: JSON_VALIDATION_FILE,
+            contentChanges: [],
+        })
+
+        sinon.assert.calledOnceWithExactly(service.doValidation, JSON_COMPLETION_FILE)
+    })
+
+    it('should complete', async () => {
+        features.workspace.getTextDocument.returns(Promise.resolve(JSON_COMPLETION_FILE))
+
+        await features.doCompletion(
+            { textDocument: JSON_COMPLETION_FILE, position: JSON_COMPLETION_POSITION },
+            CancellationToken.None
+        )
+
+        sinon.assert.calledOnceWithExactly(service.doComplete, JSON_COMPLETION_FILE, JSON_COMPLETION_POSITION)
+    })
+})
---
+++ server/aws-lsp-yaml-json/src/language-server/yamlJsonServer.ts
@@ -0,0 +1,103 @@
+import { Server } from '@aws/language-server-runtimes/out/runtimes'
+import {
+    AwsLanguageService,
+    CachedContentHandler,
+    FileHandler,
+    HttpHandler,
+    UriCacheRepository,
+    UriResolver,
+    UriResolverBuilder,
+} from '@aws/lsp-core'
+import {
+    CancellationToken,
+    CompletionList,
+    CompletionParams,
+    DidChangeTextDocumentParams,
+} from 'vscode-languageserver/node'
+import { create } from './yamlJsonService'
+
+/**
+ * This is a demonstration language server that handles both JSON and YAML files according to the
+ * CloudFormation or SAM JSON-Schema.
+ *
+ * This illustrates how we can wrap LSP Connection calls around a provided language service.
+ * In this case, the service is a composition of a JSON processor and a YAML processor.
+ */
+
+export const YamlJsonServerFactory =
+    (service: AwsLanguageService): Server =>
+    ({ credentialsProvider, lsp, workspace, telemetry, logging }) => {
+        const onInitializedHandler = async () => {}
+
+        const onCompletionHandler = async (
+            params: CompletionParams,
+            _token: CancellationToken
+        ): Promise<CompletionList | null> => {
+            const emptyCompletionList = CompletionList.create([])
+
+            // For the example, we will always return these completion items
+            const textDocument = await workspace.getTextDocument(params.textDocument.uri)
+            if (!textDocument) {
+                logging.log(`textDocument [${params.textDocument.uri}] not found`)
+                return emptyCompletionList
+            }
+            const completions = await service.doComplete(textDocument, params.position)
+            return completions ?? emptyCompletionList
+        }
+
+        const onDidChangeTextDocumentHandler = async (params: DidChangeTextDocumentParams): Promise<any> => {
+            const textDocument = await workspace.getTextDocument(params.textDocument.uri)
+            if (!textDocument) {
+                logging.log(`textDocument [${params.textDocument.uri}] not found`)
+                return
+            }
+
+            const diagnostics = await service.doValidation(textDocument)
+
+            await lsp.publishDiagnostics({
+                uri: params.textDocument.uri,
+                diagnostics: diagnostics,
+            })
+        }
+
+        lsp.onInitialized(onInitializedHandler)
+        lsp.onCompletion(onCompletionHandler)
+        lsp.onDidChangeTextDocument(onDidChangeTextDocumentHandler)
+
+        logging.log('The YAML JSON LSP Language Server has been initialised')
+
+        // disposable
+        return () => {
+            // Do nothing
+        }
+    }
+
+const jsonSchemaUrl =
+    'https://raw.githubusercontent.com/aws/serverless-application-model/main/samtranslator/schema/schema.json'
+
+function createJsonSchemaResolver(): UriResolver {
+    const builder = new UriResolverBuilder()
+
+    const cacheRepository = new UriCacheRepository()
+
+    builder
+        .addHandler(new FileHandler())
+        .addHandler(
+            new CachedContentHandler({
+                cacheRepository: cacheRepository,
+            })
+        )
+        .addHandler(new HttpHandler())
+
+    return builder.build()
+}
+
+const yamlJsonServerProps = {
+    displayName: 'aws-lsp-yaml-json',
+    defaultSchemaUri: jsonSchemaUrl,
+    uriResolver: createJsonSchemaResolver(),
+    allowComments: true,
+}
+
+const service = create(yamlJsonServerProps)
+export const YamlLanguageServer = YamlJsonServerFactory(service)
