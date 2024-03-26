440d7d05d8e621dc2ef25fb7c1610b5df51c1edf
Shailaja Yadav <shailay@amazon.com>
2024-03-26 12:47:31 -0700

    nit

--- client/vscode/src/sampleCommandActivation.ts
+++ client/vscode/src/sampleCommandActivation.ts
@@ -22,53 +22,7 @@ export function logCommand(languageClient: LanguageClient) {
 export function qNetCommand(languageClient: LanguageClient) {
     return async () => {
         const request = {
-            command: 'aws/qNetTransform/startTransform',
-            SolutionRootPath: 'D:\\TestProjects-master\\TestProjects-master\\net40\\mvc\\MvcMusicStore.sln',
-            TargetFramework: 'net8.0',
-            SourceCodeFilePaths: [
-                'D:\\TestProjects-master\\TestProjects-master\\net40\\mvc\\MvcMusicStore\\Global.asax',
-                'D:\\TestProjects-master\\TestProjects-master\\net40\\mvc\\MvcMusicStore\\Global.asax.cs',
-                'D:\\TestProjects-master\\TestProjects-master\\net40\\mvc\\MvcMusicStore\\MvcMusicStore.csproj',
-                'D:\\TestProjects-master\\TestProjects-master\\net40\\mvc\\MvcMusicStore\\MvcMusicStore.csproj.user',
-                'D:\\TestProjects-master\\TestProjects-master\\net40\\mvc\\MvcMusicStore\\Web.config',
-                'D:\\TestProjects-master\\TestProjects-master\\net40\\mvc\\MvcMusicStore\\Web.Debug.config',
-                'D:\\TestProjects-master\\TestProjects-master\\net40\\mvc\\MvcMusicStore\\Web.Release.config',
-                'D:\\TestProjects-master\\TestProjects-master\\net40\\mvc\\MvcMusicStore-Create.sql',
-                'D:\\TestProjects-master\\TestProjects-master\\net40\\mvc\\MvcMusicStore.sln',
-                'D:\\TestProjects-master\\TestProjects-master\\net40\\mvc\\UpgradeLog.htm',
-                'D:\\TestProjects-master\\TestProjects-master\\net40\\mvc\\UpgradeLog2.htm',
-            ],
-            ProjectMetadata: [
-                {
-                    Name: 'MvcMusicStore',
-                    ProjectPath:
-                        'D:\\TestProjects-master\\TestProjects-master\\net40\\mvc\\MvcMusicStore\\MvcMusicStore.csproj',
-                    ProjectLanguage: 'C#',
-                    ProjectType: '',
-                    ExternalReferences: [
-                        {
-                            ProjectPath:
-                                'D:\\TestProjects-master\\TestProjects-master\\net40\\mvc\\MvcMusicStore\\MvcMusicStore.csproj',
-                            RelativePath:
-                                'references\\Reference Assemblies\\Microsoft\\Framework\\.NETFramework\\v4.0\\System.Xml.dll',
-                            AssemblyFullPath:
-                                'C:\\Program Files (x86)\\Reference Assemblies\\Microsoft\\Framework\\.NETFramework\\v4.0\\System.Xml.dll',
-                            TargetFrameworkId: 'netframework4.0',
-                            IncludedInArtifact: false,
-                        },
-                        {
-                            ProjectPath:
-                                'D:\\TestProjects-master\\TestProjects-master\\net40\\mvc\\MvcMusicStore\\MvcMusicStore.csproj',
-                            RelativePath:
-                                'references\\Reference Assemblies\\Microsoft\\Framework\\.NETFramework\\v4.0\\System.Web.Abstractions.dll',
-                            AssemblyFullPath:
-                                'C:\\Program Files (x86)\\Reference Assemblies\\Microsoft\\Framework\\.NETFramework\\v4.0\\System.Web.Abstractions.dll',
-                            TargetFrameworkId: 'netframework4.0',
-                            IncludedInArtifact: false,
-                        },
-                    ],
-                },
-            ],
+            command: 'aws/qNetTransform/startTransform'
         }
         await languageClient.sendRequest(ExecuteCommandRequest.method, request)
         languageClient.info(`Client: The qNet command has been executed`)
