4296183068f5507b0e7f38c771a237dfd5e336c0
Oksana Volodkevych <oksanaov@amazon.com>
2024-04-15 16:01:05 +0000

    Added webpack for bundling

---
+++ chat-client/webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path')
+
+const isDevelopment = true
+
+module.exports = {
+    mode: isDevelopment ? 'development' : 'production',
+    devtool: isDevelopment ? 'inline-source-map' : undefined,
+    entry: './src/index.ts',
+    output: {
+        filename: 'amazonq-ui.js',
+        path: path.resolve(__dirname, 'build'),
+    },
+    resolve: {
+        extensions: ['.ts', '.js'],
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+        ],
+    },
+}
