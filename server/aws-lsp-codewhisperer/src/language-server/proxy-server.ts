5c5e1a2daa220ab2d0e24609bcadf5612062ade5
Kenneth Ma <16969131+makenneth@users.noreply.github.com>
2024-04-23 16:26:02 -0700

    revert changes that are not ready

--- server/aws-lsp-codewhisperer/src/language-server/proxy-server.ts
+++ server/aws-lsp-codewhisperer/src/language-server/proxy-server.ts
@@ -1,9 +1,6 @@
-import { ChatSessionManagementService } from './chat/chatSessionManagementService'
-import { ChatSessionServiceConfig } from './chat/chatSessionService'
 import { SecurityScanServerToken } from './codeWhispererSecurityScanServer'
 import { CodewhispererServerFactory } from './codeWhispererServer'
 import { CodeWhispererServiceToken } from './codeWhispererService'
-import { QChatServerToken } from './qChatServer'
 
 export const CodeWhispererServerTokenProxy = CodewhispererServerFactory(credentialsProvider => {
     let additionalAwsConfig = {}
@@ -35,26 +32,4 @@ export const CodeWhispererSecurityScanServerTokenProxy = SecurityScanServerToken
         }
     }
     return new CodeWhispererServiceToken(credentialsProvider, additionalAwsConfig)
-})
-
-export const QChatServerTokenProxy = QChatServerToken(credentialsProvider => {
-    let clientOptions: ChatSessionServiceConfig | undefined
-
-    const proxyUrl = process.env.HTTPS_PROXY ?? process.env.https_proxy
-
-    if (proxyUrl) {
-        const { getProxyHttpAgent } = require('proxy-http-agent')
-        const { NodeHttpHandler } = require('@smithy/node-http-handler')
-
-        // passing as a function so a new http handler can be created
-        clientOptions = () => ({
-            requestHandler: new NodeHttpHandler({
-                httpsAgent: getProxyHttpAgent({
-                    proxy: proxyUrl,
-                }),
-            }),
-        })
-    }
-
-    return new ChatSessionManagementService(credentialsProvider, clientOptions)
 })
