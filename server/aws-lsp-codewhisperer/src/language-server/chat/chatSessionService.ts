7314185300d48ab2742b923aaefff8cb3ab4cf81
Kenneth Ma <16969131+makenneth@users.noreply.github.com>
2024-04-23 09:28:50 -0700

    remove node specific dependency for proxy from ChatSessionService

--- server/aws-lsp-codewhisperer/src/language-server/chat/chatSessionService.ts
+++ server/aws-lsp-codewhisperer/src/language-server/chat/chatSessionService.ts
@@ -6,7 +6,6 @@ import {
 import { ConfiguredRetryStrategy } from '@aws-sdk/util-retry'
 import { CredentialsProvider } from '@aws/language-server-runtimes/server-interface'
 import { AbortController } from '@smithy/abort-controller'
-import { addProxyToClient } from 'aws-sdk-v3-proxy'
 import { getBearerTokenFromProvider } from '../utils'
 
 export class ChatSessionService {
@@ -26,20 +25,12 @@ export class ChatSessionService {
     }
 
     constructor(credentialsProvider: CredentialsProvider) {
-        const steamingClient = new CodeWhispererStreaming({
+        this.#client = new CodeWhispererStreaming({
             region: this.#codeWhispererRegion,
             endpoint: this.#codeWhispererEndpoint,
             token: () => Promise.resolve({ token: getBearerTokenFromProvider(credentialsProvider) }),
             retryStrategy: new ConfiguredRetryStrategy(0, (attempt: number) => 500 + attempt ** 10),
         })
-
-        /**
-         * Streaming client use v3 sdk so there is no longer a global configuration for proxy
-         *
-         * addProxyToClient detects environment variables and determines whether or not to attach proxy
-         */
-
-        this.#client = addProxyToClient(steamingClient, { throwOnNoProxy: false })
     }
 
     public async generateAssistantResponse(
