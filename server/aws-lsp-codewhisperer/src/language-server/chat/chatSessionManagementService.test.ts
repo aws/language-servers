535bad4f0a78a42e5d2c62d8c091505e32e0ee79
Kenneth Ma <16969131+makenneth@users.noreply.github.com>
2024-04-25 08:55:29 -0700

    Turn session management service into singleton

--- server/aws-lsp-codewhisperer/src/language-server/chat/chatSessionManagementService.test.ts
+++ server/aws-lsp-codewhisperer/src/language-server/chat/chatSessionManagementService.test.ts
@@ -5,56 +5,70 @@ import { ChatSessionManagementService } from './chatSessionManagementService'
 import { ChatSessionService } from './chatSessionService'
 
 describe('ChatSessionManagementService', () => {
-    const mockCredentialsProvider: CredentialsProvider = {
-        hasCredentials: sinon.stub().returns(true),
-        getCredentials: sinon.stub().returns(Promise.resolve({ token: 'mockToken' })),
-        getConnectionMetadata: sinon.stub(),
-    }
+    it('getInstance should return the same instance if initialized', () => {
+        const instance = ChatSessionManagementService.getInstance()
+        const instance2 = ChatSessionManagementService.getInstance()
 
-    const mockSessionId = 'mockSessionId'
-    const mockSessionId2 = 'mockSessionId2'
-    let disposeStub: sinon.SinonStub
-    let chatSessionManagementService: ChatSessionManagementService
-
-    beforeEach(() => {
-        disposeStub = sinon.stub(ChatSessionService.prototype, 'dispose')
-        chatSessionManagementService = new ChatSessionManagementService(mockCredentialsProvider)
+        assert.strictEqual(instance, instance2)
     })
 
-    afterEach(() => {
-        disposeStub.restore()
+    it('creating a session without credentials provider should throw an error', () => {
+        assert.throws(() => ChatSessionManagementService.getInstance().getSession('mockSessionId'))
     })
 
-    it('getSession should return an existing client if found or create a new client if not found', () => {
-        assert.ok(!chatSessionManagementService.hasSession(mockSessionId))
-        // getSession creates a new session if not found
-        const chatClient = chatSessionManagementService.getSession(mockSessionId)
+    describe('Session interface', () => {
+        const mockCredentialsProvider: CredentialsProvider = {
+            hasCredentials: sinon.stub().returns(true),
+            getCredentials: sinon.stub().returns(Promise.resolve({ token: 'mockToken' })),
+            getConnectionMetadata: sinon.stub(),
+        }
 
-        assert.ok(chatClient instanceof ChatSessionService)
-        assert.ok(chatSessionManagementService.hasSession(mockSessionId))
+        const mockSessionId = 'mockSessionId'
+        const mockSessionId2 = 'mockSessionId2'
+        let disposeStub: sinon.SinonStub
+        let chatSessionManagementService: ChatSessionManagementService
 
-        // check if the object reference is the same to ensure we are only creating one client
-        assert.strictEqual(chatSessionManagementService.getSession(mockSessionId), chatClient)
-    })
+        beforeEach(() => {
+            disposeStub = sinon.stub(ChatSessionService.prototype, 'dispose')
+            chatSessionManagementService =
+                ChatSessionManagementService.getInstance().withCredentialsProvider(mockCredentialsProvider)
+        })
 
-    it('deleting session should dispose the chat session service and delete from map', () => {
-        chatSessionManagementService.getSession(mockSessionId)
+        afterEach(() => {
+            disposeStub.restore()
+        })
 
-        assert.ok(chatSessionManagementService.hasSession(mockSessionId))
+        it('getSession should return an existing client if found or create a new client if not found', () => {
+            assert.ok(!chatSessionManagementService.hasSession(mockSessionId))
+            // getSession creates a new session if not found
+            const chatClient = chatSessionManagementService.getSession(mockSessionId)
 
-        chatSessionManagementService.deleteSession(mockSessionId)
+            assert.ok(chatClient instanceof ChatSessionService)
+            assert.ok(chatSessionManagementService.hasSession(mockSessionId))
 
-        assert.ok(!chatSessionManagementService.hasSession(mockSessionId))
+            // check if the object reference is the same to ensure we are only creating one client
+            assert.strictEqual(chatSessionManagementService.getSession(mockSessionId), chatClient)
+        })
 
-        sinon.assert.calledOnce(disposeStub)
-    })
+        it('deleting session should dispose the chat session service and delete from map', () => {
+            chatSessionManagementService.getSession(mockSessionId)
 
-    it('disposing the chat session management should dispose all the chat session services', () => {
-        chatSessionManagementService.getSession(mockSessionId)
-        chatSessionManagementService.getSession(mockSessionId2)
+            assert.ok(chatSessionManagementService.hasSession(mockSessionId))
 
-        chatSessionManagementService.dispose()
+            chatSessionManagementService.deleteSession(mockSessionId)
 
-        sinon.assert.calledTwice(disposeStub)
+            assert.ok(!chatSessionManagementService.hasSession(mockSessionId))
+
+            sinon.assert.calledOnce(disposeStub)
+        })
+
+        it('disposing the chat session management should dispose all the chat session services', () => {
+            chatSessionManagementService.getSession(mockSessionId)
+            chatSessionManagementService.getSession(mockSessionId2)
+
+            chatSessionManagementService.dispose()
+
+            sinon.assert.calledTwice(disposeStub)
+        })
     })
 })
