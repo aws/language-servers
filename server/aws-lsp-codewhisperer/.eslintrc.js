56caa5706b134f7b530432ad86bde6f734547611
Viktor Shcherba <vshcherb@amazon.com>
2024-06-14 15:21:06 +0200

    Clear linting for web runtime target

--- server/aws-lsp-codewhisperer/.eslintrc.js
+++ server/aws-lsp-codewhisperer/.eslintrc.js
@@ -1,8 +1,18 @@
 module.exports = {
+    root: true,
     parser: '@typescript-eslint/parser',
     plugins: ['import'],
     rules: {
         'import/no-nodejs-modules': 'warn',
     },
-    ignorePatterns: ['**/*.test.ts', 'src.gen/', 'src/client/**/*.d.ts'],
+    ignorePatterns: ['**/*.test.ts', 'out/', 'src.gen/', 'src/client/**/*.d.ts'],
+    overrides: [
+        {
+            // Lint webworker bundle does not use browser-incompatible NodeJS APIs in source code and upstream dependencies
+            files: ['bundle/aws-lsp-codewhisperer-webworker.js'],
+            rules: {
+                'import/no-nodejs-modules': 'error',
+            },
+        },
+    ],
 }
--- server/hello-world-lsp/.eslintrc.js
+++ server/hello-world-lsp/.eslintrc.js
@@ -1,14 +1,15 @@
 module.exports = {
     root: true,
     parser: '@typescript-eslint/parser',
+    plugins: ['import'],
     rules: {
-        // Add rules here
+        'import/no-nodejs-modules': 'warn',
     },
+    ignorePatterns: ['**/*.test.ts', 'out/'],
     overrides: [
         {
             // Lint webworker bundle does not use browser-incompatible NodeJS APIs in source code and upstream dependencies
-            files: ['out/hello-world-lsp-webworker.js'],
-            plugins: ['import'],
+            files: ['bundle/hello-world-lsp-webworker.js'],
             rules: {
                 'import/no-nodejs-modules': 'error',
             },
