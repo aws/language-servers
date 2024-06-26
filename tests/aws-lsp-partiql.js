d791f6112b989703f6df5f34be4359ecb5c44948
Viktor Shcherba <vshcherb@amazon.com>
2024-06-26 16:45:15 +0200

    Add more tests

---
+++ tests/aws-lsp-partiql.js
@@ -0,0 +1,5 @@
+// const assert = require('assert')
+// const partiqlPackage = require('@aws/lsp-partiql')
+// assert.ok(partiqlPackage.PartiQLLanguageServer)
+
+console.warn('AWS PartiQL server: Skipping test')
