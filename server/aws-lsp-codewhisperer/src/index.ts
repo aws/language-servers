2b0c54621ee172dd9a0753b9bf561141f125f6f7
Viktor Shcherba <vshcherb@amazon.com>
2024-06-26 15:42:31 +0200

    Extend Codewhisperer server packaging test

--- server/aws-lsp-codewhisperer/src/index.ts
+++ server/aws-lsp-codewhisperer/src/index.ts
@@ -1,3 +1,4 @@
 export * from './language-server/codeWhispererSecurityScanServer'
 export * from './language-server/codeWhispererServer'
 export * from './language-server/qChatServer'
+export * from './language-server/proxy-server'
