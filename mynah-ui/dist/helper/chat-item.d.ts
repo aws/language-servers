909d044b3859dd323a55b14f8a378202143c92f7
Ege Ozcan <eoozca@amazon.com>
2024-07-02 14:10:34 +0200

    Remove the temporary mynah-ui and consume correctly

--- mynah-ui/dist/helper/chat-item.d.ts
+++
@@ -1,3 +0,0 @@
-import { ChatItem } from '../static';
-export declare const chatItemHasContent: (chatItem: Partial<ChatItem>) => boolean;
-export declare const copyToClipboard: (textToSendClipboard: string, onCopied?: () => void) => Promise<void>;
--- mynah-ui/dist/helper/guid.d.ts
+++
@@ -1,5 +0,0 @@
-/*!
- * Copyright 2022 Amazon.com, Inc. or its affiliates. All Rights Reserved.
- * SPDX-License-Identifier: Apache-2.0
- */
-export declare const generateUID: () => string;
