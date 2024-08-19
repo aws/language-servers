a611c5bd6d3cc6cfaa8565c6dfdce40a7b620094
huawenm <72728725+huawenm@users.noreply.github.com>
2024-08-19 15:41:51 -0700

    bug fix: failed to upload due to cert validation failed
    throw the error to block next step transform and provide instruction

--- server/aws-lsp-codewhisperer/src/language-server/netTransform/transformHandler.ts
+++ server/aws-lsp-codewhisperer/src/language-server/netTransform/transformHandler.ts
@@ -148,9 +148,10 @@ export class TransformHandler {
 
             this.logging.log(`CodeTransform: Response from S3 Upload = ${response.statusCode}`)
         } catch (e: any) {
-            const errorMessage = (e as Error).message ?? 'Error in S3 UploadZip API call'
-
+            const error = e as Error
+            const errorMessage = `error: ${error.message || 'Error in S3 UploadZip API call'}, please see https://docs.aws.amazon.com/amazonq/latest/qdeveloper-ug/troubleshooting-code-transformation.html#project-upload-fail`
             this.logging.log(errorMessage)
+            throw new Error(errorMessage)
         }
     }
 
@@ -269,7 +270,7 @@ export class TransformHandler {
                 if (response.transformationJob?.status) {
                     this.logging.log(
                         'status is included in validExitSTatus for poll ' +
-                            validExitStatus.includes(response.transformationJob.status)
+                        validExitStatus.includes(response.transformationJob.status)
                     )
                 }
 
