434040d6efa94cebdeaea4e6d2348225aed57a41
Ke Deng <kdengone@amazon.com>
2024-08-28 07:27:23 -0700

    Revert "remove unnecessary imports"
    This reverts commit 20d4c0e2f803a88b43e0f9be38dae3324ce78f03.

--- server/aws-lsp-codewhisperer/src/language-server/netTransform/transformHandler.ts
+++ server/aws-lsp-codewhisperer/src/language-server/netTransform/transformHandler.ts
@@ -1,5 +1,6 @@
 import { CodeWhispererStreaming, ExportIntent } from '@amzn/codewhisperer-streaming'
 import { Logging, Workspace } from '@aws/language-server-runtimes/server-interface'
+import * as fs from 'fs'
 import got from 'got'
 import { v4 as uuidv4 } from 'uuid'
 import {
@@ -27,6 +28,7 @@ import {
 import * as validation from './validation'
 import path = require('path')
 import AdmZip = require('adm-zip')
+import { Console } from 'console'
 import { supportedProjects, unsupportedViewComponents } from './resources/SupportedProjects'
 import { String } from 'aws-sdk/clients/codebuild'
 import { ProjectMetadata } from 'aws-sdk/clients/lookoutvision'
