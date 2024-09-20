2420132bf67bb3657f215a884da5233477bf5f7e
Viktor Shcherba <vshcherb@amazon.com>
2024-09-20 14:49:28 +0200

    Remove UTG extraction function

--- server/aws-lsp-codewhisperer/src/language-server/supplementalContextUtil/supplementalContextUtil.ts
+++ server/aws-lsp-codewhisperer/src/language-server/supplementalContextUtil/supplementalContextUtil.ts
@@ -6,7 +6,6 @@
 // Port of implementation in AWS Toolkit for VSCode
 // https://github.com/aws/aws-toolkit-vscode/blob/9d8ddbd85f4533e539a58e76f7c46883d8e50a79/packages/core/src/codewhisperer/util/supplementalContext/supplementalContextUtil.ts
 
-import { fetchSupplementalContextForTest } from './utgUtils'
 import { fetchSupplementalContextForSrc } from './crossFileContextUtil'
 import { isTestFile } from './codeParsingUtil'
 import { CodeWhispererSupplementalContext } from '../models/model'
@@ -39,7 +38,8 @@ export async function fetchSupplementalContext(
     >
 
     if (isUtg) {
-        supplementalContextPromise = fetchSupplementalContextForTest(document, workspace, logging, cancellationToken)
+        // Not implemented.
+        return
     } else {
         supplementalContextPromise = fetchSupplementalContextForSrc(document, position, workspace, cancellationToken)
     }
@@ -60,7 +60,6 @@ export async function fetchSupplementalContext(
             }
         })
         .catch(err => {
-            // PORT_TODO: lift this error handling to codeWhispererServer caller
             if (err instanceof CancellationError) {
                 return {
                     isUtg: isUtg,
@@ -71,9 +70,7 @@ export async function fetchSupplementalContext(
                     strategy: 'Empty',
                 }
             } else {
-                // getLogger().error(
-                //     `Fail to fetch supplemental context for target file ${document.uri}: ${err}`
-                // )
+                logging.log(`Fail to fetch supplemental context for target file ${document.uri}: ${err}`)
                 return undefined
             }
         })
