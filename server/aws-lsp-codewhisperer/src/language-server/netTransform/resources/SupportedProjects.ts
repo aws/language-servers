e1b45ab081a8d0ea4573e3f2de97c424e29b402b
Aniruddha Dave <anirdave@amazon.com>
2024-10-18 10:32:58 -0700

    remove webforms from supported projects (#504)

--- server/aws-lsp-codewhisperer/src/language-server/netTransform/resources/SupportedProjects.ts
+++ server/aws-lsp-codewhisperer/src/language-server/netTransform/resources/SupportedProjects.ts
@@ -4,7 +4,6 @@ export const supportedProjects = [
     'AspNetCoreMvcRazorPage',
     'AspNetMvc',
     'AspNetWebApi',
-    'AspNetWebForms',
     'CoreWCFService',
     'CoreWCFServiceConfig',
     'WCFClient',
