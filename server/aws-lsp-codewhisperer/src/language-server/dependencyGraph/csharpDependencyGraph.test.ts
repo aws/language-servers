0b1707934bcaccc18e13bb41384231390dd5e7cf
Ege Ozcan <eoozca@amazon.com>
2024-10-28 12:31:48 +0100

    Simplify csharpDependencyGraph unit test to make it more resilient to runtimes changes

--- server/aws-lsp-codewhisperer/src/language-server/dependencyGraph/csharpDependencyGraph.test.ts
+++ server/aws-lsp-codewhisperer/src/language-server/dependencyGraph/csharpDependencyGraph.test.ts
@@ -13,21 +13,18 @@ describe('Test CsharpDependencyGraph', () => {
     const tempDirPath = path.resolve('\\Temp')
     const mockedFs = {
         copy: Sinon.mock(),
-        exists: Sinon.mock(),
         getFileSize: Sinon.mock(),
         getTempDirPath: () => tempDirPath,
         readdir: Sinon.stub(),
         readFile: Sinon.stub(),
         isFile: Sinon.mock(),
-        remove: Sinon.mock(),
-        writeFile: Sinon.stub(),
-        appendFile: Sinon.stub(),
-        mkdir: Sinon.stub(),
     }
+
     const mockedWorkspace: Workspace = {
         getTextDocument: Sinon.mock(),
         getAllTextDocuments: Sinon.mock(),
         getWorkspaceFolder: mockedGetWorkspaceFolder,
+        // @ts-ignore mockedFs doesn't need to mock every fs function for this test to work
         fs: mockedFs,
     }
 
