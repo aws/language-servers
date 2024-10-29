df84ad443382c51b3ee6b2e4bdd7948075c1f323
Natalia Durlik <natalia.a.durlik@gmail.com>
2024-10-29 13:53:42 +0100

    Update workspace fs interface to use expected defaults

--- server/aws-lsp-codewhisperer/src/language-server/dependencyGraph/dependencyGraph.ts
+++ server/aws-lsp-codewhisperer/src/language-server/dependencyGraph/dependencyGraph.ts
@@ -186,7 +186,7 @@ export abstract class DependencyGraph {
      */
     protected async removeDir(dir: string) {
         if (await this.workspace.fs.exists(dir)) {
-            await this.workspace.fs.remove(dir)
+            await this.workspace.fs.rm(dir, { recursive: true, force: true })
         }
     }
 
