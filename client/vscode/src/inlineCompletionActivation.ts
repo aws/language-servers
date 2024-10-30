f28341b72de51844779645193d15ca7e628ee4b5
Viktor Shcherba <vshcherb@amazon.com>
2024-10-30 18:33:50 +0100

    VSCode Client: add manual inline trigger and logSe ssionResults notification

--- client/vscode/src/inlineCompletionActivation.ts
+++ client/vscode/src/inlineCompletionActivation.ts
@@ -6,40 +6,76 @@ import {
     InlineCompletionList,
     Position,
     TextDocument,
+    commands,
     languages,
 } from 'vscode'
 import { LanguageClient } from 'vscode-languageclient/node'
-import { InlineCompletionParams, inlineCompletionWithReferencesRequestType } from './futureTypes'
+import {
+    InlineCompletionItemWithReferences,
+    InlineCompletionListWithReferences,
+    InlineCompletionWithReferencesParams,
+    inlineCompletionWithReferencesRequestType,
+    logInlineCompletionSessionResultsNotificationType,
+    LogInlineCompletionSessionResultsParams,
+} from '@aws/language-server-runtimes/protocol'
+
+export const CodewhispererInlineCompletionLanguages = [
+    { scheme: 'file', language: 'typescript' },
+    { scheme: 'file', language: 'json' },
+    { scheme: 'file', language: 'yaml' },
+    { scheme: 'file', language: 'java' },
+    { scheme: 'file', language: 'go' },
+    { scheme: 'file', language: 'php' },
+    { scheme: 'file', language: 'rust' },
+    { scheme: 'file', language: 'kotlin' },
+    { scheme: 'file', language: 'terraform' },
+    { scheme: 'file', language: 'ruby' },
+    { scheme: 'file', language: 'shellscript' },
+    { scheme: 'file', language: 'dart' },
+    { scheme: 'file', language: 'lua' },
+    { scheme: 'file', language: 'powershell' },
+    { scheme: 'file', language: 'r' },
+    { scheme: 'file', language: 'swift' },
+    { scheme: 'file', language: 'systemverilog' },
+    { scheme: 'file', language: 'scala' },
+    { scheme: 'file', language: 'vue' },
+]
 
 export function registerInlineCompletion(languageClient: LanguageClient) {
     const inlineCompletionProvider = new CodeWhispererInlineCompletionItemProvider(languageClient)
-    languages.registerInlineCompletionItemProvider(
-        [
-            { scheme: 'file', language: 'typescript' },
-            { scheme: 'file', language: 'json' },
-            { scheme: 'file', language: 'yaml' },
-            { scheme: 'file', language: 'java' },
-            { scheme: 'file', language: 'go' },
-            { scheme: 'file', language: 'php' },
-            { scheme: 'file', language: 'rust' },
-            { scheme: 'file', language: 'kotlin' },
-            { scheme: 'file', language: 'terraform' },
-            { scheme: 'file', language: 'ruby' },
-            { scheme: 'file', language: 'shellscript' },
-            { scheme: 'file', language: 'dart' },
-            { scheme: 'file', language: 'lua' },
-            { scheme: 'file', language: 'powershell' },
-            { scheme: 'file', language: 'r' },
-            { scheme: 'file', language: 'swift' },
-            { scheme: 'file', language: 'systemverilog' },
-            { scheme: 'file', language: 'scala' },
-            { scheme: 'file', language: 'vue' },
-        ],
-        inlineCompletionProvider
-    )
+    languages.registerInlineCompletionItemProvider(CodewhispererInlineCompletionLanguages, inlineCompletionProvider)
+
+    commands.registerCommand('aws.sample-vscode-ext-amazonq.invokeInlineCompletion', async (...args: any) => {
+        // Register manual trigger command for InlineCompletions
+        await commands.executeCommand(`editor.action.inlineSuggest.trigger`)
+    })
+
+    // Simple implementation of logInlineCompletionSessionResultsNotification
+    const onInlineAcceptance = async (
+        sessionId: string,
+        itemId: string,
+        requestStartTime: number,
+        firstCompletionDisplayLatency?: number
+    ) => {
+        console.log('OnInlineAcceptance called with: ', sessionId, itemId)
+        const params: LogInlineCompletionSessionResultsParams = {
+            sessionId: sessionId,
+            completionSessionResult: {
+                [itemId]: {
+                    seen: true,
+                    accepted: true,
+                    discarded: false,
+                },
+            },
+            totalSessionDisplayTime: Date.now() - requestStartTime,
+            firstCompletionDisplayLatency: firstCompletionDisplayLatency,
+        }
+        languageClient.sendNotification(logInlineCompletionSessionResultsNotificationType, params)
+    }
+    commands.registerCommand('aws.sample-vscode-ext-amazonq.accept', onInlineAcceptance)
 }
 
-class CodeWhispererInlineCompletionItemProvider implements InlineCompletionItemProvider {
+export class CodeWhispererInlineCompletionItemProvider implements InlineCompletionItemProvider {
     constructor(private readonly languageClient: LanguageClient) {}
 
     async provideInlineCompletionItems(
@@ -48,7 +84,8 @@ class CodeWhispererInlineCompletionItemProvider implements InlineCompletionItemP
         context: InlineCompletionContext,
         token: CancellationToken
     ): Promise<InlineCompletionItem[] | InlineCompletionList> {
-        const request: InlineCompletionParams = {
+        const requestStartTime = Date.now()
+        const request: InlineCompletionWithReferencesParams = {
             textDocument: {
                 uri: document.uri.toString(),
             },
@@ -62,9 +99,21 @@ class CodeWhispererInlineCompletionItemProvider implements InlineCompletionItemP
             token
         )
 
-        const list: InlineCompletionList = response as InlineCompletionList
+        const list: InlineCompletionListWithReferences = response as InlineCompletionListWithReferences
         this.languageClient.info(`Client: Received ${list.items.length} suggestions`)
 
-        return list
+        console.log('Got inlineCompletionsWithReferences from server', list)
+
+        const firstCompletionDisplayLatency = Date.now() - requestStartTime
+        // Add completion session tracking and attach onAcceptance command to each item to record used decision
+        list.items.forEach((item: InlineCompletionItemWithReferences) => {
+            item.command = {
+                command: 'aws.sample-vscode-ext-amazonq.accept',
+                title: 'On acceptance',
+                arguments: [list.sessionId, item.itemId, requestStartTime, firstCompletionDisplayLatency],
+            }
+        })
+
+        return list as InlineCompletionList
     }
 }
