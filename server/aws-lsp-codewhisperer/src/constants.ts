4ac7955b268663f35ac7baf7c7623e4aebe715ed
Ivan Mykhailovskyi <imykhai@amazon.com>
2024-10-31 10:08:37 +0000

    Move codewhisperer endpoint and region into a constants.ts

---
+++ server/aws-lsp-codewhisperer/src/constants.ts
@@ -0,0 +1,2 @@
+export const AWS_Q_ENDPOINT_URL = process?.env.AWS_Q_ENDPOINT_URL ?? 'https://codewhisperer.us-east-1.amazonaws.com/'
+export const AWS_Q_REGION = process?.env.AWS_Q_REGION ?? 'us-east-1'
