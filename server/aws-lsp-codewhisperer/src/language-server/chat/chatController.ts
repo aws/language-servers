315c39bd63aecdb4b66340c811269b450c49e28f
Josh Pinkney <jpink@amazon.com>
2025-01-24 10:32:07 -0500

    feat(amazonq): add base chat controller that can be extended

--- server/aws-lsp-codewhisperer/src/language-server/chat/chatController.ts
+++ server/aws-lsp-codewhisperer/src/language-server/chat/chatController.ts
@@ -1,25 +1,12 @@
 import { SendMessageCommandInput, SendMessageCommandOutput } from '@amzn/codewhisperer-streaming'
-import {
-    ApplyWorkspaceEditParams,
-    ErrorCodes,
-    FeedbackParams,
-    InsertToCursorPositionParams,
-    TextDocumentEdit,
-    TextEdit,
-    chatRequestType,
-} from '@aws/language-server-runtimes/protocol'
+import { ErrorCodes, FeedbackParams, chatRequestType } from '@aws/language-server-runtimes/protocol'
 import {
     CancellationToken,
-    Chat,
     ChatParams,
     ChatResult,
-    EndChatParams,
     LSPErrorCodes,
     QuickActionParams,
     ResponseError,
-    TabAddParams,
-    TabRemoveParams,
-    TabChangeParams,
 } from '@aws/language-server-runtimes/server-interface'
 import { v4 as uuid } from 'uuid'
 import {
@@ -28,7 +15,7 @@ import {
     ChatTelemetryEventName,
     CombinedConversationEvent,
 } from '../telemetry/types'
-import { Features, LspHandlers, Result } from '../types'
+import { Features, Result } from '../types'
 import { ChatEventParser, ChatResultWithMetadata } from './chatEventParser'
 import { createAuthFollowUpResult, getAuthFollowUpType, getDefaultChatResponse } from './utils'
 import { ChatSessionManagementService } from './chatSessionManagementService'
@@ -38,45 +25,40 @@ import { getErrorMessage, isAwsError, isNullish, isObject } from '../utils'
 import { Metric } from '../telemetry/metric'
 import { QChatTriggerContext, TriggerContext } from './contexts/triggerContext'
 import { HELP_MESSAGE } from './constants'
-import { Q_CONFIGURATION_SECTION } from '../configuration/qConfigurationServer'
-import { undefinedIfEmpty } from '../utilities/textUtils'
 import { TelemetryService } from '../telemetryService'
+import { BaseController } from './baseController'
 
-type ChatHandlers = LspHandlers<Chat>
-
-export class ChatController implements ChatHandlers {
-    #features: Features
-    #chatSessionManagementService: ChatSessionManagementService
+export class ChatController extends BaseController {
     #telemetryController: ChatTelemetryController
     #triggerContext: QChatTriggerContext
     #customizationArn?: string
-    #telemetryService: TelemetryService
 
     constructor(
         chatSessionManagementService: ChatSessionManagementService,
         features: Features,
         telemetryService: TelemetryService
     ) {
-        this.#features = features
-        this.#chatSessionManagementService = chatSessionManagementService
+        super(chatSessionManagementService, features, telemetryService)
         this.#triggerContext = new QChatTriggerContext(features.workspace, features.logging)
         this.#telemetryController = new ChatTelemetryController(features, telemetryService)
-        this.#telemetryService = telemetryService
     }
 
-    dispose() {
-        this.#chatSessionManagementService.dispose()
+    override dispose() {
+        super.dispose()
         this.#telemetryController.dispose()
     }
 
-    async onChatPrompt(params: ChatParams, token: CancellationToken): Promise<ChatResult | ResponseError<ChatResult>> {
+    override async onChatPrompt(
+        params: ChatParams,
+        token: CancellationToken
+    ): Promise<ChatResult | ResponseError<ChatResult>> {
         const maybeDefaultResponse = getDefaultChatResponse(params.prompt.prompt)
 
         if (maybeDefaultResponse) {
             return maybeDefaultResponse
         }
 
-        const sessionResult = this.#chatSessionManagementService.getSession(params.tabId)
+        const sessionResult = this.chatSessionManagementService.getSession(params.tabId)
 
         const { data: session, success } = sessionResult
 
@@ -92,7 +74,7 @@ export class ChatController implements ChatHandlers {
         const isNewConversation = !session.conversationId
 
         token.onCancellationRequested(() => {
-            this.#log('cancellation requested')
+            this.log('cancellation requested')
             session.abortRequest()
         })
 
@@ -101,12 +83,12 @@ export class ChatController implements ChatHandlers {
 
         const conversationIdentifier = session?.conversationId ?? 'New conversation'
         try {
-            this.#log('Request for conversation id:', conversationIdentifier)
+            this.log('Request for conversation id:', conversationIdentifier)
             requestInput = this.#triggerContext.getChatParamsFromTrigger(params, triggerContext, this.#customizationArn)
 
             metric.recordStart()
             response = await session.sendMessage(requestInput)
-            this.#log('Response for conversation id:', conversationIdentifier, JSON.stringify(response.$metadata))
+            this.log('Response for conversation id:', conversationIdentifier, JSON.stringify(response.$metadata))
         } catch (err) {
             if (isAwsError(err) || (isObject(err) && 'statusCode' in err && typeof err.statusCode === 'number')) {
                 metric.setDimension('cwsprChatRepsonseCode', err.statusCode ?? 400)
@@ -116,12 +98,12 @@ export class ChatController implements ChatHandlers {
             const authFollowType = getAuthFollowUpType(err)
 
             if (authFollowType) {
-                this.#log(`Q auth error: ${getErrorMessage(err)}`)
+                this.log(`Q auth error: ${getErrorMessage(err)}`)
 
                 return createAuthFollowUpResult(authFollowType)
             }
 
-            this.#log(`Q api request error ${err instanceof Error ? err.message : 'unknown'}`)
+            this.log(`Q api request error ${err instanceof Error ? err.message : 'unknown'}`)
             return new ResponseError<ChatResult>(
                 LSPErrorCodes.RequestFailed,
                 err instanceof Error ? err.message : 'Unknown request error'
@@ -139,7 +121,7 @@ export class ChatController implements ChatHandlers {
             )
 
             session.conversationId = result.data?.conversationId
-            this.#log('Session conversation id:', session.conversationId || '')
+            this.log('Session conversation id:', session.conversationId || '')
 
             if (session.conversationId) {
                 this.#telemetryController.setConversationId(params.tabId, session.conversationId)
@@ -175,7 +157,7 @@ export class ChatController implements ChatHandlers {
                 ? result.data.chatResult
                 : new ResponseError<ChatResult>(LSPErrorCodes.RequestFailed, result.error)
         } catch (err) {
-            this.#log('Error encountered during response streaming:', err instanceof Error ? err.message : 'unknown')
+            this.log('Error encountered during response streaming:', err instanceof Error ? err.message : 'unknown')
 
             return new ResponseError<ChatResult>(
                 LSPErrorCodes.RequestFailed,
@@ -184,102 +166,8 @@ export class ChatController implements ChatHandlers {
         }
     }
 
-    async onCodeInsertToCursorPosition(params: InsertToCursorPositionParams) {
-        // Implementation based on https://github.com/aws/aws-toolkit-vscode/blob/1814cc84228d4bf20270574c5980b91b227f31cf/packages/core/src/amazonq/commons/controllers/contentController.ts#L38
-        if (!params.textDocument || !params.cursorPosition || !params.code) {
-            const missingParams = []
-
-            if (!params.textDocument) missingParams.push('textDocument')
-            if (!params.cursorPosition) missingParams.push('cursorPosition')
-            if (!params.code) missingParams.push('code')
-
-            this.#log(
-                `Q Chat server failed to insert code. Missing required parameters for insert code: ${missingParams.join(', ')}`
-            )
-
-            return
-        }
-
-        let cursorPosition = params.cursorPosition
-
-        const indentRange = {
-            start: { line: cursorPosition.line, character: 0 },
-            end: cursorPosition,
-        }
-        const documentContent = await this.#features.workspace.getTextDocument(params.textDocument.uri)
-        // linePrefix is the raw text that is between the start of the line and the current cursor position
-        let linePrefix = documentContent?.getText(indentRange)
-        // calculatedIndent is the indent we calculate inside this function and apply to the text to be inserted
-        let calculatedIndent = ''
-        let hasVirtualSpace = false
-
-        if (linePrefix) {
-            // If linePrefix object is not empty, there are two possibilities:
-            // Case 1: If linePrefix contains only whitespace: Use the entire linePrefix as is for the indent
-            // Case 2: If linePrefix contains non-whitespace characters: Extract leading whitespace from linePrefix (if any), ignore rest of text
-            calculatedIndent =
-                linePrefix.trim().length == 0
-                    ? linePrefix
-                    : ' '.repeat(linePrefix.length - linePrefix.trimStart().length)
-        } else if (documentContent && cursorPosition.character > 0) {
-            // When the cursor is not at the start of the line (position > 0) but there's no actual text at the indentation range
-            // It means there are virtual spaces that is being rendered by the IDE
-            // In this case, the indentation is determined by the cursorPosition
-            this.#log('Indent is nullish and the cursor position is greater than zero while inserting code')
-            calculatedIndent = ' '.repeat(cursorPosition.character)
-            hasVirtualSpace = true
-            cursorPosition.character = 0
-        }
-
-        const textWithIndent = params.code
-            .split('\n')
-            .map((line, index) => {
-                if (index === 0) {
-                    return hasVirtualSpace && line ? calculatedIndent + line : line
-                }
-                // Only indent non-empty lines
-                return line ? calculatedIndent + line : ''
-            })
-            .join('\n')
-
-        const workspaceEdit: ApplyWorkspaceEditParams = {
-            edit: {
-                documentChanges: [
-                    TextDocumentEdit.create({ uri: params.textDocument.uri, version: 0 }, [
-                        TextEdit.insert(cursorPosition, textWithIndent),
-                    ]),
-                ],
-            },
-        }
-        const applyResult = await this.#features.lsp.workspace.applyWorkspaceEdit(workspaceEdit)
-
-        if (applyResult.applied) {
-            this.#log(`Q Chat server inserted code successfully`)
-            this.#telemetryController.enqueueCodeDiffEntry({ ...params, code: textWithIndent })
-        } else {
-            this.#log(
-                `Q Chat server failed to insert code: ${applyResult.failureReason ?? 'No failure reason provided'}`
-            )
-        }
-    }
-    onCopyCodeToClipboard() {}
-
-    onEndChat(params: EndChatParams, _token: CancellationToken): boolean {
-        const { success } = this.#chatSessionManagementService.deleteSession(params.tabId)
-
-        return success
-    }
-
-    onFollowUpClicked() {}
-
-    onInfoLinkClick() {}
-
-    onLinkClick() {}
-
-    onReady() {}
-
-    onSendFeedback({ tabId, feedbackPayload }: FeedbackParams) {
-        this.#features.telemetry.emitMetric({
+    override onSendFeedback({ tabId, feedbackPayload }: FeedbackParams) {
+        this.features.telemetry.emitMetric({
             name: 'amazonq_sendFeedback',
             data: {
                 comment: JSON.stringify({
@@ -295,45 +183,10 @@ export class ChatController implements ChatHandlers {
         })
     }
 
-    onSourceLinkClick() {}
-
-    onTabAdd(params: TabAddParams) {
-        this.#telemetryController.activeTabId = params.tabId
-
-        this.#chatSessionManagementService.createSession(params.tabId)
-    }
-
-    onTabChange(params: TabChangeParams) {
-        this.#telemetryController.emitConversationMetric({
-            name: ChatTelemetryEventName.ExitFocusConversation,
-            data: {},
-        })
-
-        this.#telemetryController.activeTabId = params.tabId
-
-        this.#telemetryController.emitConversationMetric({
-            name: ChatTelemetryEventName.EnterFocusConversation,
-            data: {},
-        })
-    }
-
-    onTabRemove(params: TabRemoveParams) {
-        if (this.#telemetryController.activeTabId === params.tabId) {
-            this.#telemetryController.emitConversationMetric({
-                name: ChatTelemetryEventName.ExitFocusConversation,
-                data: {},
-            })
-            this.#telemetryController.activeTabId = undefined
-        }
-
-        this.#chatSessionManagementService.deleteSession(params.tabId)
-        this.#telemetryController.removeConversation(params.tabId)
-    }
-
-    onQuickAction(params: QuickActionParams, _cancellationToken: CancellationToken) {
+    override onQuickAction(params: QuickActionParams, _cancellationToken: CancellationToken) {
         switch (params.quickAction) {
             case QuickAction.Clear: {
-                const sessionResult = this.#chatSessionManagementService.getSession(params.tabId)
+                const sessionResult = this.chatSessionManagementService.getSession(params.tabId)
 
                 this.#telemetryController.emitChatMetric({
                     name: ChatTelemetryEventName.RunCommand,
@@ -413,7 +266,7 @@ export class ChatController implements ChatHandlers {
             }
 
             if (!isNullish(partialResultToken)) {
-                await this.#features.lsp.sendProgress(chatRequestType, partialResultToken, result.data.chatResult)
+                await this.features.lsp.sendProgress(chatRequestType, partialResultToken, result.data.chatResult)
             }
         }
 
@@ -426,29 +279,5 @@ export class ChatController implements ChatHandlers {
         })
 
         return chatEventParser.getResult()
-    }
-
-    updateConfiguration = async () => {
-        try {
-            const qConfig = await this.#features.lsp.workspace.getConfiguration(Q_CONFIGURATION_SECTION)
-            if (qConfig) {
-                this.#customizationArn = undefinedIfEmpty(qConfig.customization)
-                this.#log(`Chat configuration updated to use ${this.#customizationArn}`)
-                /*
-                    The flag enableTelemetryEventsToDestination is set to true temporarily. It's value will be determined through destination
-                    configuration post all events migration to STE. It'll be replaced by qConfig['enableTelemetryEventsToDestination'] === true
-                */
-                // const enableTelemetryEventsToDestination = true
-                // this.#telemetryService.updateEnableTelemetryEventsToDestination(enableTelemetryEventsToDestination)
-                const optOutTelemetryPreference = qConfig['optOutTelemetry'] === true ? 'OPTOUT' : 'OPTIN'
-                this.#telemetryService.updateOptOutPreference(optOutTelemetryPreference)
-            }
-        } catch (error) {
-            this.#log(`Error in GetConfiguration: ${error}`)
-        }
-    }
-
-    #log(...messages: string[]) {
-        this.#features.logging.log(messages.join(' '))
     }
 }
