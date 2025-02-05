e2df7b72967a5ef86b0c987bba0ad4c668513ace
Francesco Piccoli <70111810+francescoopiccoli@users.noreply.github.com>
2025-02-05 14:51:44 +0100

    refactor: remove proxy code (moved to runtimes) (#774)
    * refactor: remove proxy code (moved to runtimes)
    
    * chore: merge branch 'main' into frapicc/remove-sdk-proxy-logic
    ---------
    
    Co-authored-by: Francesco Piccoli <frapicc@amazon.com>

--- server/aws-lsp-codewhisperer/src/client/streamingClient/codewhispererStreamingClient.ts
+++ server/aws-lsp-codewhisperer/src/client/streamingClient/codewhispererStreamingClient.ts
@@ -1,8 +1,5 @@
 import { CodeWhispererStreaming, CodeWhispererStreamingClientConfig } from '@amzn/codewhisperer-streaming'
 import { ConfiguredRetryStrategy } from '@aws-sdk/util-retry'
-import { readFileSync } from 'fs'
-import { NodeHttpHandler } from '@smithy/node-http-handler'
-import { HttpsProxyAgent } from 'hpagent'
 import { SDKInitializator } from '@aws/language-server-runtimes/server-interface'
 
 export class StreamingClient {
@@ -31,36 +28,11 @@ export async function createStreamingClient(
 ): Promise<CodeWhispererStreaming> {
     const creds = credentialsProvider.getCredentials('bearer')
 
-    let clientOptions
-    // short term solution to fix webworker bundling, broken due to this node.js specific logic in here
-    const isNodeJS: boolean = typeof process !== 'undefined' && process.release && process.release.name === 'node'
-    const proxyUrl = isNodeJS ? (process.env.HTTPS_PROXY ?? process.env.https_proxy) : undefined
-    const certs = isNodeJS
-        ? process.env.AWS_CA_BUNDLE
-            ? [readFileSync(process.env.AWS_CA_BUNDLE)]
-            : undefined
-        : undefined
-
-    if (proxyUrl) {
-        const agent = new HttpsProxyAgent({
-            proxy: proxyUrl,
-            ca: certs,
-        })
-
-        clientOptions = {
-            requestHandler: new NodeHttpHandler({
-                httpAgent: agent,
-                httpsAgent: agent,
-            }),
-        }
-    }
-
     const streamingClient = sdkInitializator(CodeWhispererStreaming, {
         region: codeWhispererRegion,
         endpoint: codeWhispererEndpoint,
         token: { token: creds.token },
         retryStrategy: new ConfiguredRetryStrategy(0, (attempt: number) => 500 + attempt ** 10),
-        requestHandler: clientOptions?.requestHandler,
         ...config,
     })
     return streamingClient
