6fa320b36ade1066d51b2d62108ffa90e5bf77cc
Paras <pmiparas@amazon.com>
2025-02-26 21:33:14 +0100

    chore: fix to pass correct headers to uploadS3 function

--- server/aws-lsp-codewhisperer/src/language-server/workspaceContext/util.ts
+++ server/aws-lsp-codewhisperer/src/language-server/workspaceContext/util.ts
@@ -20,21 +20,20 @@ export const findWorkspaceRootFolder = (
 
 export const uploadArtifactToS3 = async (content: Buffer, md5Content: string, resp: CreateUploadUrlResponse) => {
     const encryptionContext = `{"uploadId":"${resp.uploadId}"}`
-    const headersObj =
-        resp.kmsKeyArn !== '' || resp.kmsKeyArn !== undefined
-            ? {
-                  'Content-MD5': md5Content,
-                  'x-amz-server-side-encryption': 'aws:kms',
-                  'Content-Type': 'application/zip',
-                  'x-amz-server-side-encryption-aws-kms-key-id': resp.kmsKeyArn,
-                  'x-amz-server-side-encryption-context': Buffer.from(encryptionContext, 'utf8').toString('base64'),
-              }
-            : {
-                  'Content-MD5': md5Content,
-                  'x-amz-server-side-encryption': 'aws:kms',
-                  'Content-Type': 'application/zip',
-                  'x-amz-server-side-encryption-context': Buffer.from(encryptionContext, 'utf8').toString('base64'),
-              }
+    let headersObj = resp.requestHeaders
+        ? {
+              'x-amz-checksum-sha256': resp.requestHeaders['x-amz-checksum-sha256'],
+              'x-amz-expected-bucket-owner': resp.requestHeaders['x-amz-expected-bucket-owner'],
+              'Content-Type': resp.requestHeaders['content-type'],
+          }
+        : {}
+    if (resp.kmsKeyArn !== '' || resp.kmsKeyArn !== undefined) {
+        Object.assign(headersObj, {
+            'x-amz-server-side-encryption': 'aws:kms',
+            'x-amz-server-side-encryption-aws-kms-key-id': resp.kmsKeyArn,
+            'x-amz-server-side-encryption-context': Buffer.from(encryptionContext, 'utf8').toString('base64'),
+        })
+    }
     const response = await got.put(resp.uploadUrl, {
         body: content,
         headers: headersObj,
