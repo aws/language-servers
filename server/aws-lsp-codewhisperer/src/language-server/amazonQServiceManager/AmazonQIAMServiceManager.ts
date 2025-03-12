c494a0ff8b1de69bfebd8486c85b8f5d73af839b
Viktor Shcherba <vshcherb@amazon.com>
2025-03-12 16:20:46 +0100

    feat(amazonq): extend global SDK service lifecycle management and handle profile changes

---
+++ server/aws-lsp-codewhisperer/src/language-server/amazonQServiceManager/AmazonQIAMServiceManager.ts
@@ -0,0 +1,91 @@
+import {
+    Logging,
+    Lsp,
+    CredentialsProvider,
+    SDKInitializator,
+    Runtime,
+    Workspace,
+} from '@aws/language-server-runtimes/server-interface'
+import { DEFAULT_AWS_Q_ENDPOINT_URL, DEFAULT_AWS_Q_REGION } from '../../constants'
+import { CodeWhispererServiceIAM } from '../codeWhispererService'
+import {
+    AmazonQServiceNotInitializedError,
+    AmazonQServicePendingProfileError,
+    AmazonQServicePendingSigninError,
+} from './errors'
+
+interface Features {
+    lsp: Lsp
+    logging: Logging
+    runtime: Runtime
+    credentialsProvider: CredentialsProvider
+    sdkInitializator: SDKInitializator
+    workspace: Workspace
+}
+
+export class AmazonQIAMServiceManager {
+    private static instance: AmazonQIAMServiceManager | null = null
+    private features?: Features
+    private logging?: Logging
+    private codewhispererService?: CodeWhispererServiceIAM
+    private serviceStatus: 'UNINITIALIZED' | 'PENDING_SIGNIN' | 'PENDING_Q_PROFILE' | 'INITIALIZED' = 'UNINITIALIZED'
+
+    private constructor() {}
+
+    public static getInstance(features: Features): AmazonQIAMServiceManager {
+        if (!AmazonQIAMServiceManager.instance) {
+            AmazonQIAMServiceManager.instance = new AmazonQIAMServiceManager()
+            AmazonQIAMServiceManager.instance.initialize(features)
+        }
+        return AmazonQIAMServiceManager.instance
+    }
+
+    private initialize(features: Features): void {
+        this.features = features
+        this.logging = features.logging
+
+        this.setupCodewhispererService()
+    }
+
+    private setupCodewhispererService() {
+        if (!this.features) {
+            throw new Error('Features not initialized')
+        }
+
+        // Starting with default region.
+        const awsQRegion = this.features.runtime.getConfiguration('AWS_Q_REGION') ?? DEFAULT_AWS_Q_REGION
+        const awsQEndpointUrl =
+            this.features.runtime.getConfiguration('AWS_Q_ENDPOINT_URL') ?? DEFAULT_AWS_Q_ENDPOINT_URL
+
+        this.codewhispererService = new CodeWhispererServiceIAM(
+            this.features.credentialsProvider,
+            this.features.workspace,
+            awsQRegion,
+            awsQEndpointUrl,
+            this.features.sdkInitializator
+        )
+        this.serviceStatus = 'INITIALIZED'
+
+        return
+    }
+
+    public getCodewhispererService(): CodeWhispererServiceIAM {
+        if (this.serviceStatus === 'UNINITIALIZED') {
+            throw new AmazonQServiceNotInitializedError()
+        }
+
+        if (this.serviceStatus === 'PENDING_SIGNIN') {
+            throw new AmazonQServicePendingSigninError()
+        }
+
+        if (this.serviceStatus === 'PENDING_Q_PROFILE') {
+            throw new AmazonQServicePendingProfileError()
+        }
+
+        if (!this.codewhispererService) {
+            throw new AmazonQServiceNotInitializedError()
+        }
+
+        return this.codewhispererService
+    }
+}
---
+++ server/aws-lsp-codewhisperer/src/language-server/amazonQServiceManager/errors.ts
@@ -0,0 +1,62 @@
+import { AwsResponseError, ResponseError } from '@aws/language-server-runtimes/protocol'
+
+// Base error class for Amazon Q
+export class AmazonQError extends Error {
+    public code: string
+    constructor(message: string, code: string) {
+        super(message)
+        this.name = 'AmazonQError'
+        this.code = code
+    }
+}
+
+export class AmazonQConnectionError extends AmazonQError {
+    constructor(message: string = 'Failed to connect to Amazon Q service') {
+        super(message, 'E_AMAZON_Q_CONNECTION')
+        this.name = 'AmazonQConnectionError'
+    }
+}
+
+export class AmazonQAuthenticationError extends AmazonQError {
+    constructor(message: string = 'Failed to authenticate with Amazon Q service') {
+        super(message, 'E_AMAZON_Q_AUTHENTICATION')
+        this.name = 'AmazonQAuthenticationError'
+    }
+}
+
+export class AmazonQConfigurationError extends AmazonQError {
+    constructor(message: string = 'Invalid Amazon Q configuration') {
+        super(message, 'E_AMAZON_Q_CONFIGURATION')
+        this.name = 'AmazonQConfigurationError'
+    }
+}
+
+export class AmazonQServiceNotInitializedError extends AmazonQError {
+    constructor(message: string = 'Amazon Q service SDK is not initialized') {
+        super(message, 'E_AMAZON_Q_NOT_INITIALIZED')
+        this.name = 'AmazonQServiceNotInitializedError'
+    }
+}
+
+export class AmazonQServicePendingSigninError extends AmazonQError {
+    constructor(message: string = 'Amazon Q service is not signed in') {
+        super(message, 'E_AMAZON_Q_PENDING_SIGNIN')
+        this.name = 'AmazonQServicePendingSigninError'
+    }
+}
+
+export class AmazonQServicePendingProfileError extends AmazonQError {
+    constructor(message: string = 'Amazon Q Profile is not selected for IDC connection type') {
+        super(message, 'E_AMAZON_Q_PENDING_PROFILE')
+        this.name = 'AmazonQServicePendingProfileError'
+    }
+}
+
+export class AmazonQServiceInvalidProfileError extends AmazonQError {
+    constructor(message: string = 'Selected Amazon Q Profile is invalid') {
+        super(message, 'E_AMAZON_Q_INVALID_PROFILE')
+        this.name = 'AmazonQServiceInvalidProfileError'
+    }
+}
+
+export type AmazonQLspResponseError = AwsResponseError
