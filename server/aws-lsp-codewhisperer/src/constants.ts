83133d61815c5acfba7ead1c87d0aaef206e72d4
Viktor Shcherba <vshcherb@amazon.com>
2025-03-25 18:06:33 +0100

    feat(amazonq): add eu-central-1 endpoint

--- server/aws-lsp-codewhisperer/src/constants.ts
+++ server/aws-lsp-codewhisperer/src/constants.ts
@@ -3,6 +3,7 @@ export const DEFAULT_AWS_Q_REGION = 'us-east-1'
 
 export const AWS_Q_ENDPOINTS = {
     [DEFAULT_AWS_Q_REGION]: DEFAULT_AWS_Q_ENDPOINT_URL,
+    'eu-central-1': 'https://q.eu-central-1.amazonaws.com/',
 }
 
 export const AWS_Q_REGION_ENV_VAR = 'AWS_Q_REGION'
