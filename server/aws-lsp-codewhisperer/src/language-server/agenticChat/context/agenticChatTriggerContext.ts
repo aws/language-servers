69db2bd8dd631af226c5c96115e4102825019b0c
Hweinstock <42325418+Hweinstock@users.noreply.github.com>
2025-04-20 07:26:39 -0400

    fix: fallback to fs if document context fails to sync (#1017)
    * fix: fallback to fs to get textDocument
    
    * test: add tests for logic
    
    * style: avoid unnecessary mock
    
    * docs: add proper docstring
    
    * fix: update test client to sync all files

--- server/aws-lsp-codewhisperer/src/language-server/agenticChat/context/agenticChatTriggerContext.ts
+++ server/aws-lsp-codewhisperer/src/language-server/agenticChat/context/agenticChatTriggerContext.ts
@@ -20,10 +20,12 @@ import {
     InlineChatParams,
     QuickActionCommand,
     FileList,
+    TextDocument,
 } from '@aws/language-server-runtimes/server-interface'
 import { Features } from '../../types'
 import { DocumentContext, DocumentContextExtractor } from '../../chat/contexts/documentContext'
 import { workspaceUtils } from '@aws/lsp-core'
+import { URI } from 'vscode-uri'
 
 export interface TriggerContext extends Partial<DocumentContext> {
     userIntent?: UserIntent
@@ -116,8 +118,10 @@ export class AgenticChatTriggerContext {
     ): Promise<DocumentContext | undefined> {
         const { textDocument: textDocumentIdentifier, cursorState } = input
 
-        const textDocument =
-            textDocumentIdentifier?.uri && (await this.#workspace.getTextDocument(textDocumentIdentifier.uri))
+        if (textDocumentIdentifier?.uri === undefined) {
+            return
+        }
+        const textDocument = await this.getTextDocument(textDocumentIdentifier.uri)
 
         return textDocument
             ? this.#documentContextExtractor.extractDocumentContext(
@@ -127,6 +131,28 @@ export class AgenticChatTriggerContext {
                   cursorState?.[0] ?? AgenticChatTriggerContext.DEFAULT_CURSOR_STATE
               )
             : undefined
+    }
+
+    /**
+     * Fetch the current textDocument such that:
+     * 1. If the document is synced with LSP, return the synced textDocument
+     * 2. If the document is not synced with LSP, read the file from the file system
+     * 3. If the file cannot be read, return undefined
+     * @param uri
+     * @returns
+     */
+    async getTextDocument(uri: string) {
+        // Note: version is unused, and languageId can be determined from file extension.
+        const syncedTextDocument = await this.#workspace.getTextDocument(uri)
+        if (syncedTextDocument) {
+            return syncedTextDocument
+        }
+        try {
+            const content = await this.#workspace.fs.readFile(URI.parse(uri).fsPath)
+            return TextDocument.create(uri, '', 0, content)
+        } catch {
+            return
+        }
     }
 
     #guessIntentFromPrompt(prompt?: string): UserIntent | undefined {
