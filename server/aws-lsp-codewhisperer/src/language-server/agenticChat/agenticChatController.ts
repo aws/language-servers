70a086a823fc56dcd068dee0fa3147cb06684b9a
Kamiel Wanrooij <kamiel.wanrooij@gmail.com>
2025-04-22 16:16:15 +0200

    fix: improve chat rendering if there are additional chat messages (#1039)
    Co-authored-by: Kamiel Wanrooij <kamielw@amazon.com>

--- server/aws-lsp-codewhisperer/src/language-server/agenticChat/agenticChatController.ts
+++ server/aws-lsp-codewhisperer/src/language-server/agenticChat/agenticChatController.ts
@@ -442,6 +442,7 @@ export class AgenticChatController implements ChatHandlers {
                         break
                     default:
                         await chatResultStream.writeResultBlock({
+                            type: 'tool',
                             body: `${executeToolMessage(toolUse)}`,
                             messageId: toolUse.toolUseId,
                         })
@@ -475,7 +476,11 @@ export class AgenticChatController implements ChatHandlers {
                         await chatResultStream.writeResultBlock(chatResult)
                         break
                     default:
-                        await chatResultStream.writeResultBlock({ body: toolResultMessage(toolUse, result) })
+                        await chatResultStream.writeResultBlock({
+                            type: 'tool',
+                            body: toolResultMessage(toolUse, result),
+                            messageId: toolUse.toolUseId,
+                        })
                         break
                 }
             } catch (err) {
@@ -1046,6 +1051,7 @@ export class AgenticChatController implements ChatHandlers {
         const requestId = response.$metadata.requestId!
         const chatEventParser = new AgenticChatEventParser(requestId, metric)
         const streamWriter = chatResultStream.getResultStreamWriter()
+
         for await (const chatEvent of response.generateAssistantResponseResponse!) {
             const result = chatEventParser.processPartialEvent(chatEvent, contextList)
 
@@ -1054,7 +1060,9 @@ export class AgenticChatController implements ChatHandlers {
                 return result
             }
 
-            await streamWriter.write(result.data.chatResult)
+            if (chatEvent.assistantResponseEvent) {
+                await streamWriter.write(result.data.chatResult)
+            }
         }
         await streamWriter.close()
 
