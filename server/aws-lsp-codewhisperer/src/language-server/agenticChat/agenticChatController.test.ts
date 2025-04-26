c16b439d1bad94e118fc421609decc2f0f2ba058
Zoe Lin <zoelin@amazon.com>
2025-04-26 08:50:32 -0700

    fix: extra thinking in the end

--- server/aws-lsp-codewhisperer/src/language-server/agenticChat/agenticChatController.test.ts
+++ server/aws-lsp-codewhisperer/src/language-server/agenticChat/agenticChatController.test.ts
@@ -894,7 +894,7 @@ describe('AgenticChatController', () => {
 
             const chatResult = await chatResultPromise
 
-            sinon.assert.callCount(testFeatures.lsp.sendProgress, mockChatResponseList.length + 2) // response length + 2 loading messages
+            sinon.assert.callCount(testFeatures.lsp.sendProgress, mockChatResponseList.length + 1) // response length + 1 loading messages
             assert.deepStrictEqual(chatResult, {
                 additionalMessages: [],
                 body: '\n\nHello World!',
@@ -911,7 +911,7 @@ describe('AgenticChatController', () => {
 
             const chatResult = await chatResultPromise
 
-            sinon.assert.callCount(testFeatures.lsp.sendProgress, mockChatResponseList.length + 2) // response length + 2 loading message
+            sinon.assert.callCount(testFeatures.lsp.sendProgress, mockChatResponseList.length + 1) // response length + 1 loading message
             assert.deepStrictEqual(chatResult, {
                 additionalMessages: [],
                 body: '\n\nHello World!',
