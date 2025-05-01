81df44eeba5d76f35df282f27abf14d38ae61146
Supraja Venkatesh <suprajav@amazon.com>
2025-05-01 11:41:02 +0200

    feat: adding a check before invoking workspace context

--- server/aws-lsp-codewhisperer/src/language-server/chat/chatController.ts
+++ server/aws-lsp-codewhisperer/src/language-server/chat/chatController.ts
@@ -86,7 +86,7 @@ export class ChatController implements ChatHandlers {
     ) {
         this.#features = features
         this.#chatSessionManagementService = chatSessionManagementService
-        this.#triggerContext = new QChatTriggerContext(features.workspace, features.logging)
+        this.#triggerContext = new QChatTriggerContext(features.workspace, features.logging, amazonQServiceManager)
         this.#telemetryController = new ChatTelemetryController(features, telemetryService)
         this.#telemetryService = telemetryService
         this.#amazonQServiceManager = amazonQServiceManager
