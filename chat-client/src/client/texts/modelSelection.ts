0ef18511966fcd4258bee3024e53a16c433f9bfe
Tai Lai <tlaiamz@amazon.com>
2025-05-09 15:46:46 -0700

    update modelId values

--- chat-client/src/client/texts/modelSelection.ts
+++ chat-client/src/client/texts/modelSelection.ts
@@ -2,10 +2,15 @@ import { ChatItemFormItem } from '@aws/mynah-ui'
 
 export type Region = 'us-east-1' | 'eu-central-1'
 
+export enum BedrockModel {
+    CLAUDE_3_7_SONNET_20250219_V1_0 = 'CLAUDE_3_7_SONNET_20250219_V1_0',
+    CLAUDE_3_5_SONNET_20241022_V2_0 = 'CLAUDE_3_5_SONNET_20241022_V2_0',
+}
+
 const baseModelOptions: { value: string; label: string }[] = [
     { label: 'Automatic', value: 'auto' },
-    { label: 'Claude Sonnet 3.7', value: 'us.anthropic.claude-3-7-sonnet-20250219-v1:0' },
-    { label: 'Claude Sonnet 3.5', value: 'us.anthropic.claude-3-5-sonnet-20241022-v2:0' },
+    { label: 'Claude Sonnet 3.7', value: BedrockModel.CLAUDE_3_7_SONNET_20250219_V1_0 },
+    { label: 'Claude Sonnet 3.5', value: BedrockModel.CLAUDE_3_5_SONNET_20241022_V2_0 },
 ]
 
 export const baseModelSelection: ChatItemFormItem = {
