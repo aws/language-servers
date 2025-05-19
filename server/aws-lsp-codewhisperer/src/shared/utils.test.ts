4ed474b0fbd27ef7b5a1d1b69ffcd1071a9cdbd7
Na Yue <yueny@amazon.com>
2025-05-19 13:27:11 -0700

    Revert "fix: re-categorize error status code (#1355)"
    This reverts commit a98a842fb5ac8d680e973d97058c22a49e5c3284.

--- server/aws-lsp-codewhisperer/src/shared/utils.test.ts
+++ server/aws-lsp-codewhisperer/src/shared/utils.test.ts
@@ -8,7 +8,6 @@ import {
     getEndPositionForAcceptedSuggestion,
     safeGet,
     isStringOrNull,
-    getHttpStatusCode,
 } from './utils'
 import { expect } from 'chai'
 import { BUILDER_ID_START_URL } from './constants'
@@ -239,42 +238,5 @@ describe('isStringOrNull', () => {
         it(`should return: ${testCase.expected}, when passed: ${JSON.stringify(testCase.input)}`, () => {
             assert(isStringOrNull(testCase.input) === testCase.expected)
         })
-    })
-})
-
-describe('getHttpStatusCode', () => {
-    it('should return 500 for InputTooLong errors', () => {
-        const err = { code: 'InputTooLong' }
-        expect(getHttpStatusCode(err)).to.equal(500)
-    })
-
-    it('should return 400 for unactionable errors', () => {
-        const err = { code: 'PromptCharacterLimit' }
-        expect(getHttpStatusCode(err)).to.equal(400)
-    })
-
-    it('should return status code from $response', () => {
-        const err = { $response: { statusCode: 403 } }
-        expect(getHttpStatusCode(err)).to.equal(403)
-    })
-
-    it('should return status code from $metadata', () => {
-        const err = { $metadata: { httpStatusCode: 404 } }
-        expect(getHttpStatusCode(err)).to.equal(404)
-    })
-
-    it('should return status code from cause.$metadata', () => {
-        const err = { cause: { $metadata: { httpStatusCode: 429 } } }
-        expect(getHttpStatusCode(err)).to.equal(429)
-    })
-
-    it('should return 500 when no status code is found', () => {
-        const err = { message: 'Some error' }
-        expect(getHttpStatusCode(err)).to.equal(500)
-    })
-
-    it('should return 500 for null or undefined', () => {
-        expect(getHttpStatusCode(null)).to.equal(500)
-        expect(getHttpStatusCode(undefined)).to.equal(500)
     })
 })
