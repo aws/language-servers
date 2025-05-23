f15e95fe56066cc89357f620cba3cf7de6290766
Randall-Jiang <haoranj@amazon.com>
2025-05-23 13:25:55 -0700

    fix: add validation for user's input (#1434)

--- chat-client/src/client/mynahUi.ts
+++ chat-client/src/client/mynahUi.ts
@@ -1475,7 +1475,6 @@ ${params.message}`,
                     }
                 },
             }
-
             mynahUi.openDetailedList({ detailedList, events }, true)
         } else if (params.id === 'open-mcp-server') {
             //turning off splash loader in case of being on when new server is added
