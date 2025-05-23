4c34a241fe19338bb6790a2d485b52eccd729016
Ashish Reddy Podduturi <ashishrp@amazon.com>
2025-05-23 18:59:44 -0500

    fix: fix for imports in agenticchatController

--- server/aws-lsp-codewhisperer/src/language-server/agenticChat/agenticChatController.ts
+++ server/aws-lsp-codewhisperer/src/language-server/agenticChat/agenticChatController.ts
@@ -125,19 +125,11 @@ import {
 } from './constants'
 import { AgenticChatError, customerFacingErrorCodes, unactionableErrorCodes } from './errors'
 import { URI } from 'vscode-uri'
-import { MCP_SERVER_STATUS_CHANGED, McpManager } from './tools/mcp/mcpManager'
+import { McpManager } from './tools/mcp/mcpManager'
 import { McpTool } from './tools/mcp/mcpTool'
-import {
-    getGlobalMcpConfigPath,
-    getGlobalPersonaConfigPath,
-    getWorkspaceMcpConfigPaths,
-    getWorkspacePersonaConfigPaths,
-    processMcpToolUseMessage,
-} from './tools/mcp/mcpUtils'
 import { CommandCategory } from './tools/executeBash'
 import { UserWrittenCodeTracker } from '../../shared/userWrittenCodeTracker'
 import { McpEventHandler } from './tools/mcp/mcpEventHandler'
-import { McpServerRuntimeState } from './tools/mcp/mcpTypes'
 
 type ChatHandlers = Omit<
     LspHandlers<Chat>,
