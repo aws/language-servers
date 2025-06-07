0b6372c5e1901bd109fdb4e5dcff79fcd1912350
Chris Long <longachr@amazon.com>
2025-06-06 17:21:42 -0700

    feat: add packageId property to references in req.json

--- server/aws-lsp-codewhisperer/src/language-server/netTransform/artifactManager.ts
+++ server/aws-lsp-codewhisperer/src/language-server/netTransform/artifactManager.ts
@@ -188,7 +188,7 @@ export class ArtifactManager {
         )
         if (thirdPartyPackage) {
             artifactReference.isThirdPartyPackage = true
-
+            artifactReference.packageId = thirdPartyPackage.Id
             if (thirdPartyPackage.NetCompatibleAssemblyRelativePath && thirdPartyPackage.NetCompatibleAssemblyPath) {
                 const privatePackageRelativePath = path
                     .join(
