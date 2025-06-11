ca3a012e98ee4bb2a144f1dfeb390f10e73f9d67
Dhanasekar Karuppasamy <dhanak@amazon.com>
2025-06-10 22:55:53 -0700

    fix: remove dockerfiles. can be added as devexp later

--- docker/build-codewhisperer.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Build the Docker image without using cache
-docker-compose build --no-cache
-
-# Run a container to copy the built file
-docker-compose run --rm language-servers-build cp /output/aws-lsp-codewhisperer.js /host-output/
-
-echo "Build complete! The aws-lsp-codewhisperer.js file has been copied to the parent directory."
