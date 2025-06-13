d611003914d95c1fcc03064bb5ce587a149504c7
Manodnya Bhoite <manodnyb@amazon.com>
2025-06-13 11:11:07 -0700

    fix: remove /manage options from the chat prompt popup

--- server/aws-lsp-codewhisperer/src/language-server/agenticChat/agenticChatController.ts
+++ server/aws-lsp-codewhisperer/src/language-server/agenticChat/agenticChatController.ts
@@ -2502,19 +2502,6 @@ export class AgenticChatController implements ChatHandlers {
                     body: HELP_MESSAGE,
                 }
 
-            // "Manage Subscription" (paid-tier user), or "Upgrade Q" (free-tier user)
-            case QuickAction.Manage:
-                this.#telemetryController.emitChatMetric({
-                    name: ChatTelemetryEventName.RunCommand,
-                    data: {
-                        cwsprChatCommandType: params.quickAction,
-                        cwsprChatCommandName: '/manage',
-                    },
-                })
-
-                void this.onManageSubscription(params.tabId)
-
-                return {}
             default:
                 return {}
         }
