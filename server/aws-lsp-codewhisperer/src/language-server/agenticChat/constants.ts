9b10b8ab54620cfd0c742a1428ae3f157a3594cf
Tai Lai <tlaiamz@amazon.com>
2025-06-13 18:14:40 -0700

    feat: update list of models and set default to 4

--- server/aws-lsp-codewhisperer/src/language-server/agenticChat/constants.ts
+++ server/aws-lsp-codewhisperer/src/language-server/agenticChat/constants.ts
@@ -5,6 +5,4 @@ export const outputLimitExceedsPartialMsg = 'output exceeds maximum character li
 export const responseTimeoutMs = 170_000
 export const responseTimeoutPartialMsg = 'Response processing timed out after'
 export const clientTimeoutMs = 180_000
-export const defaultModelId = undefined
-// TODO: Uncomment this line when model selection is ready to release
-// export const defaultModelId = 'CLAUDE_3_7_SONNET_20250219_V1_0' // TODO: this can't be imported from chat-client, so we hardcode it here
+export const defaultModelId = 'CLAUDE_SONNET_4_20250514_V1_0' // TODO: this can't be imported from chat-client, so we hardcode it here
