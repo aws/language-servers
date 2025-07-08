86d6d139a1b4c4dcde9682053fe2a0a38bf5539c
abhraina-aws <abhraina@amazon.com>
2025-07-08 13:29:27 -0700

    refactor(amazonq): futher constant handling and creation of a helper (#1850)

--- server/aws-lsp-codewhisperer/src/language-server/agenticChat/constants/modelSelection.ts
+++ server/aws-lsp-codewhisperer/src/language-server/agenticChat/constants/modelSelection.ts
@@ -9,19 +9,19 @@ type ModelDetails = {
     label: string
 }
 
-const modelRecord: Record<BedrockModel, ModelDetails> = {
+const MODEL_RECORD: Record<BedrockModel, ModelDetails> = {
     [BedrockModel.CLAUDE_3_7_SONNET_20250219_V1_0]: { label: 'Claude Sonnet 3.7' },
     [BedrockModel.CLAUDE_SONNET_4_20250514_V1_0]: { label: 'Claude Sonnet 4' },
 }
 
-export const modelOptions: ListAvailableModelsResult['models'] = Object.entries(modelRecord).map(
+export const MODEL_OPTIONS: ListAvailableModelsResult['models'] = Object.entries(MODEL_RECORD).map(
     ([value, { label }]) => ({
         id: value,
         name: label,
     })
 )
 
-export const modelOptionsForRegion: Record<string, ListAvailableModelsResult['models']> = {
-    'us-east-1': modelOptions,
-    'eu-central-1': modelOptions.filter(option => option.id !== BedrockModel.CLAUDE_SONNET_4_20250514_V1_0),
+export const MODEL_OPTIONS_FOR_REGION: Record<string, ListAvailableModelsResult['models']> = {
+    'us-east-1': MODEL_OPTIONS,
+    'eu-central-1': MODEL_OPTIONS.filter(option => option.id !== BedrockModel.CLAUDE_SONNET_4_20250514_V1_0),
 }
