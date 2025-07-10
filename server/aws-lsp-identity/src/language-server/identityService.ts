cb3e01bdf547ac5cd1cbba08b702a567652d871a
Ramon Li <liramon@amazon.com>
2025-07-10 17:31:10 -0400

    feat(identity): add support for retrieving IAM user credentials

--- server/aws-lsp-identity/src/language-server/identityService.ts
+++ server/aws-lsp-identity/src/language-server/identityService.ts
@@ -3,16 +3,22 @@ import {
     AwsBuilderIdSsoTokenSource,
     AwsErrorCodes,
     CancellationToken,
+    GetIamCredentialParams,
+    GetIamCredentialResult,
     getSsoTokenOptionsDefaults,
+    getIamCredentialOptionsDefaults,
     GetSsoTokenParams,
     GetSsoTokenResult,
+    IamCredentials,
     IamIdentityCenterSsoTokenSource,
     InvalidateSsoTokenParams,
     InvalidateSsoTokenResult,
     MetricEvent,
     SsoSession,
     SsoTokenSourceKind,
+    ProfileKind,
 } from '@aws/language-server-runtimes/server-interface'
+
 import { normalizeSettingList, ProfileStore } from './profiles/profileService'
 import { authorizationCodePkceFlow, awsBuilderIdReservedName, awsBuilderIdSsoRegion } from '../sso'
 import { SsoCache, SsoClientRegistration } from '../sso/cache'
@@ -24,9 +30,11 @@ import {
     SsoFlowParams,
 } from '../sso/utils'
 import { AwsError, Observability } from '@aws/lsp-core'
+import { GetCallerIdentityCommand, STSClient } from '@aws-sdk/client-sts'
 import { __ServiceException } from '@aws-sdk/client-sso-oidc/dist-types/models/SSOOIDCServiceException'
 import { deviceCodeFlow } from '../sso/deviceCode/deviceCodeFlow'
 import { SSOToken } from '@smithy/shared-ini-file-loader'
+import { IAMClient, SimulatePrincipalPolicyCommand } from '@aws-sdk/client-iam'
 
 type SsoTokenSource = IamIdentityCenterSsoTokenSource | AwsBuilderIdSsoTokenSource
 type AuthFlows = Record<AuthorizationFlowKind, (params: SsoFlowParams) => Promise<SSOToken>>
@@ -132,6 +140,64 @@ export class IdentityService {
         } catch (e) {
             emitMetric('Failed', e, ssoSession, clientRegistration)
 
+            throw e
+        }
+    }
+
+    async getIamCredential(params: GetIamCredentialParams, token: CancellationToken): Promise<GetIamCredentialResult> {
+        const emitMetric = this.emitMetric.bind(
+            this,
+            'flareIdentity_getIamCredential',
+            this.getIamCredential.name,
+            Date.now()
+        )
+
+        try {
+            const options = { ...getIamCredentialOptionsDefaults, ...params.options }
+
+            token.onCancellationRequested(_ => {
+                if (options.generateOnInvalidStsCredential) {
+                    emitMetric('Cancelled', null)
+                }
+            })
+
+            // Get the profile with provided name
+            const profileData = await this.profileStore.load()
+            const profile = profileData.profiles.find(p => p.name === params.profileName)
+            if (!profile) {
+                this.observability.logging.log('Profile not found.')
+                throw new AwsError('Profile not found.', AwsErrorCodes.E_PROFILE_NOT_FOUND)
+            }
+
+            let credentials: IamCredentials
+            // Get the credentials directly from the profile
+            if (profile.kinds.includes(ProfileKind.IamUserProfile)) {
+                credentials = {
+                    accessKeyId: profile.settings!.aws_access_key_id!,
+                    secretAccessKey: profile.settings!.aws_secret_access_key!,
+                    sessionToken: profile.settings!.aws_session_token!,
+                }
+            } else {
+                throw new AwsError('Credentials could not be found for profile', AwsErrorCodes.E_INVALID_PROFILE)
+            }
+
+            // Validate permissions on user or assumed role
+            const hasPermissions = await this.validatePermissions(credentials, profile.settings?.region)
+            if (!hasPermissions) {
+                throw new AwsError(
+                    `User or assumed role has insufficient permissions.`,
+                    AwsErrorCodes.E_INVALID_PROFILE
+                )
+            }
+
+            emitMetric('Succeeded')
+            return {
+                id: profile.name,
+                credentials: credentials,
+                updateCredentialsParams: { data: credentials, encrypted: false },
+            }
+        } catch (e) {
+            emitMetric('Failed', e)
             throw e
         }
     }
@@ -246,5 +312,55 @@ export class IdentityService {
         ;(ssoSession.settings ||= {}).sso_registration_scopes = ssoSession.settings.sso_registration_scopes
 
         return ssoSession
+    }
+
+    // Returns whether the identity associated with the provided credentials has sufficient permissions
+    private async validatePermissions(
+        credentials: IamCredentials,
+        region: string | undefined
+    ): Promise<boolean | undefined> {
+        // Get the identity associated with the credentials
+        const stsClient = new STSClient({ region: region || 'us-east-1', credentials: credentials })
+        const identity = await stsClient.send(new GetCallerIdentityCommand({}))
+        if (!identity.Arn) {
+            throw new AwsError('Caller identity ARN not found.', AwsErrorCodes.E_INVALID_PROFILE)
+        }
+
+        // Check the permissions attached to the identity
+        const iamClient = new IAMClient({ region: region || 'us-east-1', credentials: credentials })
+        const response = await iamClient.send(
+            new SimulatePrincipalPolicyCommand({
+                PolicySourceArn: this.convertToIamArn(identity.Arn),
+                ActionNames: [
+                    'q:StartConversation',
+                    'q:SendMessage',
+                    'q:GetConversation',
+                    'q:ListConversations',
+                    'q:UpdateConversation',
+                    'q:DeleteConversation',
+                    'q:PassRequest',
+                    'q:StartTroubleshootingAnalysis',
+                    'q:StartTroubleshootingResolutionExplanation',
+                    'q:GetTroubleshootingResults',
+                    'q:UpdateTroubleshootingCommandResult',
+                    'q:GetIdentityMetaData',
+                    'q:GenerateCodeFromCommands',
+                    'q:UsePlugin',
+                    'codewhisperer:GenerateRecommendations',
+                ],
+            })
+        )
+        return response.EvaluationResults?.every(result => result.EvalDecision === 'allowed')
+    }
+
+    // Converts an assumed role ARN into an IAM role ARN
+    private convertToIamArn(arn: string) {
+        if (arn.includes(':assumed-role/')) {
+            const parts = arn.split(':')
+            const roleName = parts[5].split('/')[1]
+            return `arn:aws:iam::${parts[4]}:role/${roleName}`
+        } else {
+            return arn
+        }
     }
 }
