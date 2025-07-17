2b76c0d17b7a3c61f4876b258ced0f4bb5083209
Ramon Li <liramon@amazon.com>
2025-07-17 16:20:57 -0400

    refactor: move unit tests from identityService to iamProvider

--- server/aws-lsp-identity/src/language-server/identityService.test.ts
+++ server/aws-lsp-identity/src/language-server/identityService.test.ts
@@ -8,6 +8,7 @@ import { createStubInstance, restore, spy, SinonSpy, stub } from 'sinon'
 import {
     AuthorizationFlowKind,
     CancellationToken,
+    IamCredentials,
     ProfileKind,
     SsoTokenSourceKind,
 } from '@aws/language-server-runtimes/protocol'
@@ -16,9 +17,8 @@ import { Logging, Telemetry } from '@aws/language-server-runtimes/server-interfa
 import { Observability } from '@aws/lsp-core'
 import { StsCache } from '../sts/cache/stsCache'
 import { StsAutoRefresher } from '../sts/stsAutoRefresher'
-import { STSClient } from '@aws-sdk/client-sts'
-import { IAMClient } from '@aws-sdk/client-iam'
 import { IamProvider } from '../iam/iamProvider'
+import * as iamUtils from '../iam/utils'
 
 // eslint-disable-next-line
 use(require('chai-as-promised'))
@@ -30,7 +30,7 @@ let ssoCache: StubbedInstance<SsoCache>
 let stsCache: StubbedInstance<StsCache>
 let autoRefresher: StubbedInstance<SsoTokenAutoRefresher>
 let stsAutoRefresher: StubbedInstance<StsAutoRefresher>
-let iamProvider: IamProvider
+let iamProvider: StubbedInstance<IamProvider>
 let observability: StubbedInstance<Observability>
 let authFlowFn: SinonSpy
 
@@ -52,79 +52,6 @@ describe('IdentityService', () => {
                         settings: {
                             aws_access_key_id: 'my-access-key',
                             aws_secret_access_key: 'my-secret-key',
-                        },
-                    },
-                    {
-                        kinds: [ProfileKind.IamCredentialsProfile],
-                        name: 'my-sts-profile',
-                        settings: {
-                            aws_access_key_id: 'my-access-key',
-                            aws_secret_access_key: 'my-secret-key',
-                            aws_session_token: 'my-session-token',
-                        },
-                    },
-                    {
-                        kinds: [ProfileKind.IamSourceProfileProfile],
-                        name: 'my-role-profile',
-                        settings: {
-                            role_arn: 'my-role-arn',
-                            source_profile: 'my-iam-profile',
-                        },
-                    },
-                    {
-                        kinds: [ProfileKind.IamSourceProfileProfile],
-                        name: 'my-mfa-profile',
-                        settings: {
-                            role_arn: 'my-role-arn',
-                            source_profile: 'my-iam-profile',
-                            mfa_serial: 'my-device-arn',
-                        },
-                    },
-                    {
-                        kinds: [ProfileKind.IamCredentialProcessProfile],
-                        name: 'my-process-profile',
-                        settings: {
-                            credential_process: 'my-process',
-                        },
-                    },
-                    {
-                        kinds: [ProfileKind.IamRoleSourceProfile],
-                        name: 'cyclic-profile-1',
-                        settings: {
-                            role_arn: 'my-role-arn',
-                            source_profile: 'cyclic-profile-1',
-                        },
-                    },
-                    {
-                        kinds: [ProfileKind.IamRoleSourceProfile],
-                        name: 'cyclic-profile-2',
-                        settings: {
-                            role_arn: 'my-role-arn',
-                            source_profile: 'cyclic-profile-3',
-                        },
-                    },
-                    {
-                        kinds: [ProfileKind.IamRoleSourceProfile],
-                        name: 'cyclic-profile-3',
-                        settings: {
-                            role_arn: 'my-role-arn',
-                            source_profile: 'cyclic-profile-2',
-                        },
-                    },
-                    {
-                        kinds: [ProfileKind.IamRoleSourceProfile],
-                        name: 'base-profile',
-                        settings: {
-                            role_arn: 'my-role-arn',
-                            source_profile: 'intermediate-profile',
-                        },
-                    },
-                    {
-                        kinds: [ProfileKind.IamRoleSourceProfile],
-                        name: 'intermediate-profile',
-                        settings: {
-                            role_arn: 'my-role-arn',
-                            source_profile: 'my-iam-profile',
                         },
                     },
                 ],
@@ -170,7 +97,12 @@ describe('IdentityService', () => {
             unwatch: undefined,
         }) as StubbedInstance<StsAutoRefresher>
 
-        iamProvider = new IamProvider()
+        iamProvider = createStubInstance(IamProvider, {
+            getCredential: Promise.resolve({
+                accessKeyId: 'access-key',
+                secretAccessKey: 'secret-key',
+            } as IamCredentials),
+        }) as StubbedInstance<IamProvider>
 
         authFlowFn = spy(() =>
             Promise.resolve({
@@ -204,21 +136,8 @@ describe('IdentityService', () => {
             }
         )
 
-        stub(STSClient.prototype, 'send').resolves({
-            Credentials: {
-                AccessKeyId: 'role-access-key',
-                SecretAccessKey: 'role-secret-key',
-                SessionToken: 'role-session-token',
-                Expiration: new Date('2024-09-25T18:09:20.455Z'),
-            },
-            AssumedRoleUser: {
-                Arn: 'role-arn',
-                AssumedRoleId: 'role-id',
-            },
-            Arn: 'role-arn',
-        })
-
-        stub(IAMClient.prototype, 'send').resolves({
+        stub(iamUtils, 'simulatePermissions').resolves({
+            $metadata: {},
             EvaluationResults: [],
         })
     })
@@ -380,88 +299,11 @@ describe('IdentityService', () => {
     })
 
     describe('getIamCredential', () => {
-        it('Can login with access key, secret key, and session token.', async () => {
-            const actual = await sut.getIamCredential({ profileName: 'my-sts-profile' }, CancellationToken.None)
+        it('Can login with IAM credentials.', async () => {
+            const actual = await sut.getIamCredential({ profileName: 'my-iam-profile' }, CancellationToken.None)
 
-            expect(actual.credential.credentials.accessKeyId).to.equal('my-access-key')
-            expect(actual.credential.credentials.secretAccessKey).to.equal('my-secret-key')
-            expect(actual.credential.credentials.sessionToken).to.equal('my-session-token')
-        })
-
-        it('Can login with assumed role.', async () => {
-            const actual = await sut.getIamCredential({ profileName: 'my-role-profile' }, CancellationToken.None)
-
-            expect(actual.credential.credentials.accessKeyId).to.equal('role-access-key')
-            expect(actual.credential.credentials.secretAccessKey).to.equal('role-secret-key')
-            expect(actual.credential.credentials.sessionToken).to.equal('role-session-token')
-            expect(actual.credential.credentials.expiration?.toISOString()).to.equal('2024-09-25T18:09:20.455Z')
-            expect(stsAutoRefresher.watch.calledOnce).to.be.true
-        })
-
-        it('Returns existing STS credential.', async () => {
-            stsCache.getStsCredential = (() =>
-                Promise.resolve({
-                    Credentials: {
-                        AccessKeyId: 'other-access-key',
-                        SecretAccessKey: 'other-secret-key',
-                        SessionToken: 'other-session-token',
-                        Expiration: new Date('2024-10-25T18:09:20.455Z'),
-                    },
-                    AssumedRoleUser: {
-                        Arn: 'other-role-arn',
-                        AssumedRoleId: 'other-role-id',
-                    },
-                })) as any
-            const actual = await sut.getIamCredential({ profileName: 'my-role-profile' }, CancellationToken.None)
-
-            expect(actual.credential.credentials.accessKeyId).to.equal('other-access-key')
-            expect(actual.credential.credentials.secretAccessKey).to.equal('other-secret-key')
-            expect(actual.credential.credentials.sessionToken).to.equal('other-session-token')
-            expect(actual.credential.credentials.expiration?.toISOString()).to.equal('2024-10-25T18:09:20.455Z')
-            expect(stsAutoRefresher.watch.calledOnce).to.be.true
-        })
-
-        it('Throws when no STS credential cached and generateOnInvalidStsCredential is false.', async () => {
-            const error = await expect(
-                sut.getIamCredential(
-                    {
-                        profileName: 'my-role-profile',
-                        options: { generateOnInvalidStsCredential: false },
-                    },
-                    CancellationToken.None
-                )
-            ).rejectedWith(Error)
-
-            expect(error.message).to.equal('STS credential not found.')
-            expect(stsAutoRefresher.watch.calledOnce).to.be.false
-        })
-
-        it('Can login with chained role source profiles.', async () => {
-            const actual = await sut.getIamCredential({ profileName: 'base-profile' }, CancellationToken.None)
-
-            expect(actual.credential.credentials.accessKeyId).to.equal('role-access-key')
-            expect(actual.credential.credentials.secretAccessKey).to.equal('role-secret-key')
-            expect(actual.credential.credentials.sessionToken).to.equal('role-session-token')
-            expect(actual.credential.credentials.expiration?.toISOString()).to.equal('2024-09-25T18:09:20.455Z')
-            expect(stsAutoRefresher.watch.called).to.be.true
-        })
-
-        it('Throws when role source profile points to itself.', async () => {
-            const error = await expect(
-                sut.getIamCredential({ profileName: 'cyclic-profile-1' }, CancellationToken.None)
-            ).rejectedWith(Error)
-
-            expect(error.message).to.equal('Source profile chain exceeded max length.')
-            expect(stsAutoRefresher.watch.calledOnce).to.be.false
-        })
-
-        it('Throws when role source profiles form cycle.', async () => {
-            const error = await expect(
-                sut.getIamCredential({ profileName: 'cyclic-profile-2' }, CancellationToken.None)
-            ).rejectedWith(Error)
-
-            expect(error.message).to.equal('Source profile chain exceeded max length.')
-            expect(stsAutoRefresher.watch.calledOnce).to.be.false
+            expect(actual.credential.credentials.accessKeyId).to.equal('access-key')
+            expect(actual.credential.credentials.secretAccessKey).to.equal('secret-key')
         })
     })
 
