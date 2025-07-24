18be6914aa86fc6c19e5c66c900e10f3c0e92be5
invictus <149003065+ashishrp-aws@users.noreply.github.com>
2025-07-24 14:49:27 -0700

    chore(amazonq): revert for emergency reverts for auto-approve feature (#1973)
    * Revert "revert: revert for all commits for emergency deployment (#1966)"
    
    This reverts commit 519f75d22466b72702793b4f1d1ed846c02bbd14.
    
    * Revert "fix(amazonq): revert commit f17b631d9e06371a11ef8e9cb1413762fb51a143 (#1965)"
    
    This reverts commit 8c2cab6995922c96030b5bbdf3cbbdef7eadd7c2.
    
    * fix: fix for CI test failure

--- server/aws-lsp-codewhisperer/src/language-server/agenticChat/agenticChatController.ts
+++ server/aws-lsp-codewhisperer/src/language-server/agenticChat/agenticChatController.ts
@@ -185,6 +185,9 @@ import {
     DEFAULT_WINDOW_REJECT_SHORTCUT,
     DEFAULT_MACOS_STOP_SHORTCUT,
     DEFAULT_WINDOW_STOP_SHORTCUT,
+    OUT_OF_WORKSPACE_WARNING_MSG,
+    CREDENTIAL_FILE_WARNING_MSG,
+    BINARY_FILE_WARNING_MSG,
 } from './constants/constants'
 import {
     AgenticChatError,
@@ -1692,9 +1695,9 @@ export class AgenticChatController implements ChatHandlers {
                         const tool = new Tool(this.#features)
 
                         // For MCP tools, get the permission from McpManager
-                        // const permission = McpManager.instance.getToolPerm('Built-in', toolUse.name)
+                        const permission = McpManager.instance.getToolPerm('Built-in', toolUse.name)
                         // If permission is 'alwaysAllow', we don't need to ask for acceptance
-                        // const builtInPermission = permission !== 'alwaysAllow'
+                        const builtInPermission = permission !== 'alwaysAllow'
 
                         // Get the approved paths from the session
                         const approvedPaths = session.approvedPaths
@@ -1705,19 +1708,40 @@ export class AgenticChatController implements ChatHandlers {
                             approvedPaths
                         )
 
-                        // Honor built-in permission if available, otherwise use tool's requiresAcceptance
-                        // const requiresAcceptance = builtInPermission || toolRequiresAcceptance
+                        const isExecuteBash = toolUse.name === EXECUTE_BASH
 
-                        if (requiresAcceptance || toolUse.name === EXECUTE_BASH) {
+                        // check if tool execution's path is out of workspace
+                        const isOutOfWorkSpace = warning === OUT_OF_WORKSPACE_WARNING_MSG
+                        // check if tool involved secured files
+                        const isSecuredFilesInvoled =
+                            warning === BINARY_FILE_WARNING_MSG || warning === CREDENTIAL_FILE_WARNING_MSG
+
+                        // Honor built-in permission if available, otherwise use tool's requiresAcceptance
+                        let toolRequiresAcceptance =
+                            (builtInPermission || isOutOfWorkSpace || isSecuredFilesInvoled) ?? requiresAcceptance
+
+                        // if the command is read-only and in-workspace --> flip back to no approval needed
+                        if (
+                            isExecuteBash &&
+                            commandCategory === CommandCategory.ReadOnly &&
+                            !isOutOfWorkSpace &&
+                            !requiresAcceptance
+                        ) {
+                            toolRequiresAcceptance = false
+                        }
+
+                        if (toolRequiresAcceptance || isExecuteBash) {
                             // for executeBash, we till send the confirmation message without action buttons
                             const confirmationResult = this.#processToolConfirmation(
                                 toolUse,
-                                requiresAcceptance,
+                                toolRequiresAcceptance,
                                 warning,
-                                commandCategory
+                                commandCategory,
+                                toolUse.name,
+                                builtInPermission
                             )
                             cachedButtonBlockId = await chatResultStream.writeResultBlock(confirmationResult)
-                            const isExecuteBash = toolUse.name === EXECUTE_BASH
+
                             if (isExecuteBash) {
                                 this.#telemetryController.emitInteractWithAgenticChat(
                                     'GeneratedCommand',
@@ -1728,7 +1752,7 @@ export class AgenticChatController implements ChatHandlers {
                                     this.#abTestingAllocation?.userVariation
                                 )
                             }
-                            if (requiresAcceptance) {
+                            if (toolRequiresAcceptance) {
                                 await this.waitForToolApproval(
                                     toolUse,
                                     chatResultStream,
@@ -2699,7 +2723,7 @@ export class AgenticChatController implements ChatHandlers {
                     body = builtInPermission
                         ? `I need permission to read files.\n${formattedPaths.join('\n')}`
                         : `I need permission to read files outside the workspace.\n${formattedPaths.join('\n')}`
-                } else {
+                } else if (toolName === 'listDirectory') {
                     const readFilePath = (toolUse.input as unknown as ListDirectoryParams).path
 
                     // Validate the path using our synchronous utility
@@ -2709,6 +2733,11 @@ export class AgenticChatController implements ChatHandlers {
                     body = builtInPermission
                         ? `I need permission to list directories.\n\`${readFilePath}\``
                         : `I need permission to list directories outside the workspace.\n\`${readFilePath}\``
+                } else {
+                    const readFilePath = (toolUse.input as unknown as ListDirectoryParams).path
+                    body = builtInPermission
+                        ? `I need permission to search files.\n\`${readFilePath}\``
+                        : `I need permission to search files outside the workspace.\n\`${readFilePath}\``
                 }
                 break
             }
