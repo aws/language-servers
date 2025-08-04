69c8c6af9f8851fc08ac78302d89c1423aaa10c4
Jason Guo <jasonguo@amazon.com>
2025-08-04 12:05:47 -0700

    fix: outdated history when trimming happens, add missing metric for compaction

--- server/aws-lsp-codewhisperer/src/language-server/agenticChat/agenticChatController.ts
+++ server/aws-lsp-codewhisperer/src/language-server/agenticChat/agenticChatController.ts
@@ -1021,11 +1021,8 @@ export class AgenticChatController implements ChatHandlers {
         if (currentMessage) {
             //  Get and process the messages from history DB to maintain invariants for service requests
             try {
-                const { messages: historyMessages, count: historyCharCount } = this.#chatHistoryDb.fixAndGetHistory(
-                    tabId,
-                    currentMessage,
-                    []
-                )
+                const { history: historyMessages, historyCount: historyCharCount } =
+                    this.#chatHistoryDb.fixAndGetHistory(tabId, conversationIdentifier ?? '', currentMessage, [])
                 messages = historyMessages
                 characterCount = historyCharCount
             } catch (err) {
@@ -1199,19 +1196,18 @@ export class AgenticChatController implements ChatHandlers {
             if (currentMessage) {
                 //  Get and process the messages from history DB to maintain invariants for service requests
                 try {
-                    const newUserInputCount = this.#chatHistoryDb.calculateNewMessageCharacterCount(
+                    const {
+                        history: historyMessages,
+                        historyCount: historyCharacterCount,
+                        currentCount: currentInputCount,
+                    } = this.#chatHistoryDb.fixAndGetHistory(
+                        tabId,
+                        conversationId,
                         currentMessage,
                         pinnedContextMessages
                     )
-                    const { messages: historyMessages, count: historyCharacterCount } =
-                        this.#chatHistoryDb.fixAndGetHistory(
-                            tabId,
-                            currentMessage,
-                            pinnedContextMessages,
-                            newUserInputCount
-                        )
                     messages = historyMessages
-                    currentRequestCount = newUserInputCount + historyCharacterCount
+                    currentRequestCount = currentInputCount + historyCharacterCount
                     this.#debug(`Request total character count: ${currentRequestCount}`)
                 } catch (err) {
                     if (err instanceof ToolResultValidationError) {
@@ -1437,6 +1433,10 @@ export class AgenticChatController implements ChatHandlers {
         }
 
         if (this.#shouldCompact(currentRequestCount)) {
+            this.#telemetryController.emitCompactNudge(
+                currentRequestCount,
+                this.#features.runtime.serverInfo.version ?? ''
+            )
             const messageId = this.#getMessageIdForCompact(uuid())
             const confirmationResult = this.#processCompactConfirmation(messageId, currentRequestCount)
             const cachedButtonBlockId = await chatResultStream.writeResultBlock(confirmationResult)
