1f2fcf5256f46a25614af224f71902911013be4e
Laxman Reddy <141967714+laileni-aws@users.noreply.github.com>
2025-08-08 09:49:49 -0700

    feat(amazonq): Fetching models from backend and adding cache implementation. (#2075)
    * fix: removing and refactoring legacy code before implementing model selection
    
    * feat(amazonq): adding cache implementation and fetching models from listAvailableModels api
    
    * feat(amazonq): adding selected model in error case
    
    * feat(amazonq): adding test cases
    
    * fix: addressing comments
    
    * fix: fixing test cases and adding modelName to models
    
    * fix: minor edits
    
    * fix: minor edits
    
    * fix: minor modifications in logs
    
    * fix: adding default model if api throws any errors
    
    * fix: refactoring code

--- server/aws-lsp-codewhisperer/src/language-server/agenticChat/constants/modelSelection.ts
+++ server/aws-lsp-codewhisperer/src/language-server/agenticChat/constants/modelSelection.ts
@@ -1,5 +1,8 @@
 import { ListAvailableModelsResult } from '@aws/language-server-runtimes/protocol'
 
+/**
+ * @deprecated Do not add new models to the enum.
+ */
 export enum BedrockModel {
     CLAUDE_SONNET_4_20250514_V1_0 = 'CLAUDE_SONNET_4_20250514_V1_0',
     CLAUDE_3_7_SONNET_20250219_V1_0 = 'CLAUDE_3_7_SONNET_20250219_V1_0',
@@ -9,19 +12,14 @@ type ModelDetails = {
     label: string
 }
 
-const MODEL_RECORD: Record<BedrockModel, ModelDetails> = {
-    [BedrockModel.CLAUDE_3_7_SONNET_20250219_V1_0]: { label: 'Claude Sonnet 3.7' },
-    [BedrockModel.CLAUDE_SONNET_4_20250514_V1_0]: { label: 'Claude Sonnet 4' },
+export const MODEL_RECORD: Record<BedrockModel, ModelDetails> = {
+    [BedrockModel.CLAUDE_3_7_SONNET_20250219_V1_0]: { label: 'claude-3.7-sonnet' },
+    [BedrockModel.CLAUDE_SONNET_4_20250514_V1_0]: { label: 'claude-4-sonnet' },
 }
 
-export const MODEL_OPTIONS: ListAvailableModelsResult['models'] = Object.entries(MODEL_RECORD).map(
+export const FALLBACK_MODEL_OPTIONS: ListAvailableModelsResult['models'] = Object.entries(MODEL_RECORD).map(
     ([value, { label }]) => ({
         id: value,
         name: label,
     })
 )
-
-export const MODEL_OPTIONS_FOR_REGION: Record<string, ListAvailableModelsResult['models']> = {
-    'us-east-1': MODEL_OPTIONS,
-    'eu-central-1': MODEL_OPTIONS,
-}
