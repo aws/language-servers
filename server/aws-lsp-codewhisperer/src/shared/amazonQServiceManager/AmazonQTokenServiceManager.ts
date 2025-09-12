0548b07e763b2440ab51fb573785e92a81c06760
Will Lo <xshaohua@amazon.com>
2025-09-12 14:26:08 -0700

    chore: profile change log is recording stale profile instead of new

--- server/aws-lsp-codewhisperer/src/shared/amazonQServiceManager/AmazonQTokenServiceManager.ts
+++ server/aws-lsp-codewhisperer/src/shared/amazonQServiceManager/AmazonQTokenServiceManager.ts
@@ -417,9 +417,9 @@ export class AmazonQTokenServiceManager extends BaseAmazonQServiceManager<
         const newRegion = newProfile.identityDetails.region
         if (oldRegion === newRegion) {
             this.log(`New profile is in the same region as old one, keeping exising service.`)
-            this.log(`New active profile is ${this.activeIdcProfile.arn}, region ${oldRegion}`)
             this.activeIdcProfile = newProfile
             this.state = 'INITIALIZED'
+            this.log(`New active profile is ${this.activeIdcProfile.arn}, region ${oldRegion}`)
 
             if (this.cachedCodewhispererService) {
                 this.cachedCodewhispererService.profileArn = newProfile.arn
