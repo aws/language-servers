937844f712aad587bfc1b6c4f76f5d488bec9205
Will Lo <xshaohua@amazon.com>
2025-09-16 12:50:12 -0700

    fix: inline UTD empty cases dont differentiate Edit and Completion

--- server/aws-lsp-codewhisperer/src/shared/codeWhispererService.ts
+++ server/aws-lsp-codewhisperer/src/shared/codeWhispererService.ts
@@ -543,20 +543,37 @@ export class CodeWhispererServiceToken extends CodeWhispererServiceBase {
 
     private mapCodeWhispererApiResponseToSuggestion(
         apiResponse: GenerateCompletionsResponse,
+        request: GenerateTokenSuggestionsRequest,
         responseContext: ResponseContext
     ): GenerateSuggestionsResponse {
-        if (apiResponse?.predictions && apiResponse.predictions.length > 0) {
-            const suggestionType = apiResponse.predictions[0].edit ? SuggestionType.EDIT : SuggestionType.COMPLETION
-            const predictionType = suggestionType === SuggestionType.COMPLETION ? 'completion' : 'edit'
+        // "Predictions" will be returned if clients speicifed predictionType in the generateCompletion request, otherwise "Completions" will be returned for the backward compatibility
+        if (apiResponse?.predictions) {
+            // Infer suggestion type from the actual suggestions if any, otherwise use client specified prediction tyoe
+            if (apiResponse.predictions.length > 0) {
+                const suggestionType = apiResponse.predictions[0].edit ? SuggestionType.EDIT : SuggestionType.COMPLETION
+                const predictionType = suggestionType === SuggestionType.COMPLETION ? 'completion' : 'edit'
 
-            return {
-                suggestions: apiResponse.predictions.map(prediction => ({
-                    content: prediction[predictionType]?.content ?? '',
-                    references: prediction[predictionType]?.references ?? [],
-                    itemId: this.generateItemId(),
-                })),
-                suggestionType,
-                responseContext,
+                return {
+                    suggestions: apiResponse.predictions.map(prediction => ({
+                        content: prediction[predictionType]?.content ?? '',
+                        references: prediction[predictionType]?.references ?? [],
+                        itemId: this.generateItemId(),
+                    })),
+                    suggestionType,
+                    responseContext,
+                }
+            } else if (apiResponse.predictions.length === 0) {
+                const requestPredictionType = request.predictionTypes
+                const suggestionType =
+                    requestPredictionType && requestPredictionType.includes('EDITS')
+                        ? SuggestionType.EDIT
+                        : SuggestionType.COMPLETION
+
+                return {
+                    suggestions: [],
+                    suggestionType: suggestionType,
+                    responseContext,
+                }
             }
         }
 
