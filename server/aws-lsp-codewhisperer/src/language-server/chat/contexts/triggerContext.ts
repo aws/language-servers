5dbfcbdec21053a80052b7e87ce136bb047c6aed
laileni <laileni@amazon.com>
2026-03-04 10:56:17 -0800

    fix: entire document info for empty files

--- server/aws-lsp-codewhisperer/src/language-server/chat/contexts/triggerContext.ts
+++ server/aws-lsp-codewhisperer/src/language-server/chat/contexts/triggerContext.ts
@@ -76,32 +76,31 @@ export class QChatTriggerContext {
                 currentMessage: {
                     userInputMessage: {
                         content: prompt.escapedPrompt ?? prompt.prompt,
-                        userInputMessageContext:
-                            triggerContext.cursorState && triggerContext.relativeFilePath
-                                ? {
-                                      editorState: {
-                                          cursorState: triggerContext.cursorState,
-                                          document: {
-                                              text: documentText,
-                                              programmingLanguage: triggerContext.programmingLanguage,
-                                              relativeFilePath: triggerContext.relativeFilePath,
-                                          },
-                                          ...(triggerContext.useRelevantDocuments && {
-                                              useRelevantDocuments: triggerContext.useRelevantDocuments,
-                                              relevantDocuments: triggerContext.relevantDocuments,
-                                          }),
+                        userInputMessageContext: triggerContext.relativeFilePath
+                            ? {
+                                  editorState: {
+                                      cursorState: triggerContext.cursorState,
+                                      document: {
+                                          text: documentText,
+                                          programmingLanguage: triggerContext.programmingLanguage,
+                                          relativeFilePath: triggerContext.relativeFilePath,
                                       },
-                                      tools,
-                                  }
-                                : {
-                                      tools,
                                       ...(triggerContext.useRelevantDocuments && {
-                                          editorState: {
-                                              useRelevantDocuments: triggerContext.useRelevantDocuments,
-                                              relevantDocuments: triggerContext.relevantDocuments,
-                                          },
+                                          useRelevantDocuments: triggerContext.useRelevantDocuments,
+                                          relevantDocuments: triggerContext.relevantDocuments,
                                       }),
                                   },
+                                  tools,
+                              }
+                            : {
+                                  tools,
+                                  ...(triggerContext.useRelevantDocuments && {
+                                      editorState: {
+                                          useRelevantDocuments: triggerContext.useRelevantDocuments,
+                                          relevantDocuments: triggerContext.relevantDocuments,
+                                      },
+                                  }),
+                              },
                         userIntent: triggerContext.userIntent,
                         origin: 'IDE',
                     },
